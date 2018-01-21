8f93bff0e35c62c311c615f0581a6736d4cf4c1f
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-21 21:56:51 +0000

    added LongestPalindromicSubstring central around approach

--- src/main/java/string/LongestPalindromicSubstring.java
+++ src/main/java/string/LongestPalindromicSubstring.java
@@ -9,11 +9,47 @@ package string;
  */
 public class LongestPalindromicSubstring {
 
-	/*
-	* added main method 
-	*/
-	public static void main(String[] args) {
+    /**
+     * central around method. efficiency: O(n^2) space: O(1)
+     * 
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome(String s) {
+        int n = s.length();
+        if (n == 1)
+            return s;
+        String answer = "";
+        for (int i = 0; i < n; i++) {
+            String subPalindrome = max(getPalindrome(i, i, s), getPalindrome(i, i + 1, s));
+            answer = max(answer, subPalindrome);
+        }
+        return answer;
+    }
 
-	}
+    private static String max(String left, String right) {
+        return left.length() > right.length() ? left : right;
+    }
+
+    public static String getPalindrome(int left, int right, String s) {
+        if (right >= s.length())
+            return "";
+        int n = s.length();
+        while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(++left, right);
+    }
+
+    /*
+     * added main method
+     */
+    public static void main(String[] args) {
+        String test1 = "cbbd";
+        System.out.println(longestPalindrome(test1) + " == bb");
+        String test2 = "ccabacd";
+        System.out.println(longestPalindrome(test2) + " == cabac");
+    }
 
 }
