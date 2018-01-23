07afedf357b925fde485687fb042bc30e7ef563a
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-23 22:18:56 +0000

    added hash set impl

---
+++ src/main/java/data/structure/HashSetImpl.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package data.structure;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author weiyan.xiang
+ * @date 23 Jan 2018
+ */
+public class HashSetImpl {
+    private Map<String, String> memory;
+    private final static String DEFAULT_VALUE = "";
+
+    public HashSetImpl() {
+        this.memory = new HashMap<>();
+    }
+
+    public boolean add(String s) {
+        return this.memory.put(s, DEFAULT_VALUE) == null;
+    }
+
+    public boolean remove(String s) {
+        return this.memory.remove(s) != null;
+    }
+
+    public boolean contains(String s) {
+        return this.memory.containsKey(s);
+    }
+
+}
