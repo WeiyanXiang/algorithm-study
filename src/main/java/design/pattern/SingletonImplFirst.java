390c26d5ac7c5164834c4eac70fd491180b22fdf
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-25 12:28:22 +0000

    added singleton design pattern

---
+++ src/main/java/design/pattern/SingletonImplFirst.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package design.pattern;
+
+/**
+ * @author weiyan.xiang
+ * @date 25 Jan 2018
+ */
+public class SingletonImplFirst {
+
+    private static SingletonImplFirst instance;
+
+    /**
+     * lazy initialization
+     * 
+     * @return
+     */
+    public static SingletonImplFirst getInstanceLazy() {
+        if (instance == null) {
+            return new SingletonImplFirst();
+        }
+        return instance;
+    }
+
+    /**
+     * thread safe
+     * 
+     * @return
+     */
+    public static synchronized SingletonImplFirst getInstanceThreadSafe() {
+        if (instance == null) {
+            return new SingletonImplFirst();
+        }
+        return instance;
+    }
+
+    /**
+     * A performance enhancement but not thread-safe
+     * 
+     * @return
+     */
+    public static SingletonImplFirst getInstanceEnhancedThreadSafe() {
+        synchronized (SingletonImplFirst.class) {
+            if (instance == null) {
+                return new SingletonImplFirst();
+            }
+            return instance;
+        }
+    }
+
+}
