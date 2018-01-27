c3616910d3ff38fdb766a4c8c51df28ac3504d05
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-27 16:25:19 +0000

    added more impl of Optional usage

--- src/main/java/knowledge/OptionalDemo.java
+++ src/main/java/knowledge/OptionalDemo.java
@@ -43,11 +43,38 @@ public class OptionalDemo {
         Optional<String> emptyIntegerInstance = Optional.empty();
         System.out.println("emptyIntegerInstance: " + emptyIntegerInstance.map(value -> value.toUpperCase()));
         System.out.println("emptyIntegerInstance: " + emptyIntegerInstance.map(String::toUpperCase));
-        Optional<List<String>> obj1 = Optional.of(Arrays.asList("t1", "t2", "t3"));
-        // System.out.println("Optional.of(\"value\"): " + obj1.map());
-        // System.out.println("Optional.ofNullable(null)): " +
-        // obj2.orElse("null"));
+        /*
+         * dif between map and flatMap
+         */
+        Optional<Optional<String>> obj1 = Optional.of(Optional.of("a string"));
+        System.out.println("Optional.map: " + obj1.map(element -> element.map(String::toUpperCase)));
+        // flatMap will fold all nested optional and return a flat one
+        System.out.println("Optional.flatMap: " + obj1.flatMap(element -> element.map(String::toUpperCase)));
+    }
 
+    public static void testOptionalFilter() {
+        Optional<String> testObj1 = Optional.empty();
+        Optional<String> testObj2 = Optional.of("a string");
+        System.out.println("Optional empty => " + testObj1.filter(element -> element.equals("A STRING")));
+        System.out.println("Optional empty => " + testObj1.filter(element -> element.equalsIgnoreCase("A STRING")));
+        System.out
+                .println("Optional [a string] => " + testObj2.filter(element -> element.equalsIgnoreCase("A STRING")));
+    }
+
+    public static void testOptionalIsPresent() {
+        Optional<String> testObj1 = Optional.empty();
+        Optional<String> testObj2 = Optional.of("a string");
+        System.out.println("false => " + testObj1.isPresent());
+        testObj2.ifPresent(element -> System.out.println("A STRING == " + element.toUpperCase()));
+    }
+
+    public static void testOptionalOrElse() {
+        Optional<String> testObj1 = Optional.empty();
+        Optional<String> testObj2 = Optional.of("a string");
+        System.out.println("empty == " + testObj1.orElse("empty"));
+        System.out.println("empty == " + testObj1.orElseGet(() -> "empty"));
+        System.out.println("a string == " + testObj2.orElse("empty"));
+        System.out.println("a string == " + testObj2.orElseGet(() -> "empty"));
     }
 
     public static void main(String[] args) {
@@ -62,11 +89,14 @@ public class OptionalDemo {
         }
         testOptionalBasic();
         testOptionalMap();
-        List<String> test = Arrays.asList("t1", "t2", "t3");
+        testOptionalFilter();
+        testOptionalIsPresent();
+        testOptionalOrElse();
 
         /*
          * a bit exploring in stream, will give a deep learning following on
          */
+        List<String> test = Arrays.asList("t1", "t2", "t3");
         test.stream().map(element -> element.toUpperCase()).forEach(System.out::println);
         test.stream().map(String::toLowerCase).distinct().sorted().forEach(System.out::println);
     }
