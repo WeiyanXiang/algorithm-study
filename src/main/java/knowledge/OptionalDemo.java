97e49e4779f2294a36b4882cf3a8e798940a8f32
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-27 10:50:46 +0000

    added more impl of Optional usage

--- src/main/java/knowledge/OptionalDemo.java
+++ src/main/java/knowledge/OptionalDemo.java
@@ -3,12 +3,34 @@
  */
 package knowledge;
 
+import java.util.Optional;
+
 /**
  * @author weiyan.xiang
  * @date 26 Jan 2018
  */
 public class OptionalDemo {
+
+    private String demoField;
+
+    /**
+     * @param demoField
+     */
+    public OptionalDemo(String demoField) {
+        this.demoField = demoField;
+    }
+
+    public Optional<String> getDemoField() {
+        return Optional.ofNullable(demoField);
+    }
+
+    public void setDemoField(String demoField) {
+        this.demoField = demoField;
+    }
+
     public static void main(String[] args) {
+        OptionalDemo testObj = new OptionalDemo(null);
+        Optional<String> demoField = testObj.getDemoField();
 
     }
 }
