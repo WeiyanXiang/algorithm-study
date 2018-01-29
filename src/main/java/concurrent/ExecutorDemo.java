14de2931703e56fb178e4cd9bbe506c2d04086a1
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-29 18:20:13 +0000

    added more impl of executor services

--- src/main/java/concurrent/ExecutorDemo.java
+++ src/main/java/concurrent/ExecutorDemo.java
@@ -3,6 +3,8 @@
  */
 package concurrent;
 
+import java.util.Arrays;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,22 +16,48 @@ import java.util.concurrent.Future;
  */
 public class ExecutorDemo {
     public static void main(String[] args) {
-        ExecutorService executor = Executors.newFixedThreadPool(3);
-        Future<?> submit1 = executor.submit(new RunnableImpl());
-        Future<String> submit2 = executor.submit(new CallalableImpl());
-        executor.execute(new RunnableImpl());
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        RunnableImpl runnableTask = new RunnableImpl();
+        CallalableImpl callalableTask = new CallalableImpl();
+
+        executorService.execute(runnableTask);
+        Future<?> submit1 = executorService.submit(runnableTask);
+        Future<String> submit2 = executorService.submit(callalableTask);
 
         try {
+            String invokeAnyReturnedValue = executorService.invokeAny(Arrays.asList(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("SECOND callable is invoked");
+                    return "SECOND callable is invoked";
+                }
+            }, new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("THIRD callable is invoked");
+                    return "THIRD callable is invoked";
+                }
+            }, new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("FOUR callable is invoked");
+                    return "FOUR callable is invoked";
+                }
+            }));
+
+            System.out.println("executorService.invokeAny method returns: " + invokeAnyReturnedValue);
+
             if (submit1.isDone()) {
                 System.out.println("executor.submit(new RunnableImpl() is done and returned: " + submit1.get());
             }
-            Thread.sleep(100);
+            Thread.sleep(400);
             if (submit2.isDone()) {
                 System.out.println("executor.submit(new CallalableImpl()) is done and returned: " + submit2.get());
             }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
+        } finally {
+            executorService.shutdown();
         }
-
     }
 }
