c3c2f6f25d0fb64d4e214c77e13cf40bc803a49c
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-29 18:31:04 +0000

    added more impl of executor services

--- src/main/java/concurrent/ExecutorDemo.java
+++ src/main/java/concurrent/ExecutorDemo.java
@@ -4,6 +4,7 @@
 package concurrent;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -44,8 +45,34 @@ public class ExecutorDemo {
                     return "FOUR callable is invoked";
                 }
             }));
-
             System.out.println("executorService.invokeAny method returns: " + invokeAnyReturnedValue);
+
+            List<Future<String>> invokeAllFutures = executorService.invokeAll(Arrays.asList(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("SECOND callable is invoked");
+                    return "5th callable is invoked";
+                }
+            }, new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("THIRD callable is invoked");
+                    return "6th callable is invoked";
+                }
+            }, new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("FOUR callable is invoked");
+                    return "7th callable is invoked";
+                }
+            }));
+            invokeAllFutures.stream().forEach(eachFuture -> {
+                try {
+                    System.out.println("Each Future after invokeAll is: " + eachFuture.get());
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            });
 
             if (submit1.isDone()) {
                 System.out.println("executor.submit(new RunnableImpl() is done and returned: " + submit1.get());
