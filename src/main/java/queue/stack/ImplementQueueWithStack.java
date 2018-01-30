49410a6a4ce635074e43f3e03fb3538be71ea7ab
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-01-30 22:43:57 +0000

    added impl queue using 2 stacks

---
+++ src/main/java/queue/stack/ImplementQueueWithStack.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package queue.stack;
+
+import java.util.Stack;
+
+/**
+ * @author weiyan.xiang
+ * @date 30 Jan 2018
+ */
+public class ImplementQueueWithStack {
+    private Stack<String> s1 = new Stack<>();
+    private Stack<String> s2 = new Stack<>();
+
+    /**
+     * dequeue
+     * 
+     * @return
+     */
+    public String poll() {
+        if (!s1.isEmpty())
+            return s1.pop();
+        return null;
+    }
+
+    public String peek() {
+        if (!s1.isEmpty())
+            return s1.peek();
+        return null;
+    }
+
+    /**
+     * enqueue, make it costly and make dequeue quick
+     */
+    public void add(String element) {
+        // move all elements in s1 to s2
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        s1.push(element);
+        // move s2 elements back to s1
+        while (!s2.isEmpty()) {
+            s1.push(s2.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        ImplementQueueWithStack queue = new ImplementQueueWithStack();
+        queue.add("1");
+        queue.add("2");
+        queue.add("3");
+        queue.add("4");
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+    }
+}
