cb70359c05320baee820a98b2e9273278a3d9707
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-01 21:31:08 +0000

    added more impl for linked list

--- src/main/java/data/structure/LinkedListImpl.java
+++ src/main/java/data/structure/LinkedListImpl.java
@@ -3,8 +3,6 @@
  */
 package data.structure;
 
-import java.util.LinkedList;
-
 /**
  * @author weiyan.xiang
  * @date 1 Feb 2018
@@ -14,6 +12,8 @@ import java.util.LinkedList;
  */
 public class LinkedListImpl {
 
+    public static Node root;
+
     /**
      * have the node def first
      * 
@@ -22,7 +22,6 @@ public class LinkedListImpl {
      */
     private static class Node {
         private int value;
-        private Node prev;
         private Node next;
 
         /**
@@ -42,14 +41,6 @@ public class LinkedListImpl {
             this.value = value;
         }
 
-        public Node getPrev() {
-            return prev;
-        }
-
-        public void setPrev(Node prev) {
-            this.prev = prev;
-        }
-
         public Node getNext() {
             return next;
         }
@@ -60,21 +51,137 @@ public class LinkedListImpl {
 
     }
 
-    // addFirst(Node element)
-    //
-    // addLast(Node element)
-    //
-    // add(int index, Node element)
-    //
-    // remove(int index)
-    //
-    // remove(Node element)
-    //
-    // Node getFirst()
-    //
-    // Node get(int index)
+    public static void addFirst(Node element) {
+        element.next = root;
+        root = element;
+    }
+
+    public static void addLast(Node element) {
+        Node node = root;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = element;
+    }
+
+    public static void add(int index, Node element) {
+        if (index == 0) {
+            addFirst(element);
+            return;
+        }
+        Node node = root;
+        int counter = 0;
+        while (counter < index - 1 && node.next != null) {
+            node = node.next;
+            counter++;
+        }
+        element.next = node.next;
+        node.next = element;
+    }
+
+    /**
+     * return element previously in the position
+     * 
+     * @param index
+     * @return
+     */
+    public static Node remove(int index) {
+        if (index == 0) {
+            root = root.next;
+            return root;
+        }
+        Node node = root;
+        int counter = 0;
+        while (counter < index - 1 && node.next != null) {
+            node = node.next;
+            counter++;
+        }
+        Node toReturn = node.next;
+        if (node.next != null) {
+            node.next = node.next.next;
+        } else {
+            node.next = null;
+        }
+        return toReturn;
+
+    }
+
+    public static boolean remove(Node element) {
+        Node node = root;
+        while (node.next != null) {
+            if (node.next.value == element.value) {
+                node.next = node.next.next;
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    public static Node getFirst() {
+        return root == null ? null : root;
+    }
+
+    public static Node get(int index) {
+        Node node = root;
+        int counter = 0;
+        while (counter < index && node.next != null) {
+            node = node.next;
+            counter++;
+        }
+        if (counter == index)
+            return node;
+        return null;
+    }
 
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        root = new Node(0);
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        root.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        /**
+         * root -> node1 -> node2 -> node3 -> node4 -> node5
+         */
+        Node newNode = new Node(100);
+        addFirst(newNode);
+        printLinkedList(newNode);
+
+        Node lastNode = new Node(200);
+        addLast(lastNode);
+        printLinkedList(root);
+
+        remove(7);
+        remove(0);
+        printLinkedList(root);
+
+        Node nodeToRemove = new Node(3);
+        remove(nodeToRemove);
+        printLinkedList(root);
+
+        System.out.println("current root value is: " + getFirst().value);
+        System.out.println("current value at index 2 is: " + get(2).value);
+        System.out.println("current value at index 5 is: " + get(10));
+
+        Node testNode = new Node(3);
+        add(3, testNode);
+        printLinkedList(root);
+
+    }
+
+    private static void printLinkedList(Node newNode) {
+        System.out.println();
+        Node element = newNode;
+        while (element != null) {
+            System.out.println("Node value: " + element.value);
+            element = element.next;
+        }
     }
 }
