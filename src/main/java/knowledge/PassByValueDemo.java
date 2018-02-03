d8bcde85cc5e4df0034cfe2ca81ca68de7678ab3
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-03 23:17:10 +0000

    added more examples forpass by value demo

--- src/main/java/knowledge/PassByValueDemo.java
+++ src/main/java/knowledge/PassByValueDemo.java
@@ -9,6 +9,19 @@ public class PassByValueDemo {
 
     private int value = 0;
 
+    public class TestPoint {
+
+        private String name;
+
+        /**
+         * @param name
+         */
+        public TestPoint(String name) {
+            this.name = name;
+        }
+
+    }
+
     public void increase(int input) {
         value = input;
     }
@@ -17,6 +30,24 @@ public class PassByValueDemo {
         PassByValueDemo instance = new PassByValueDemo();
         instance.increase(3);
         System.out.println(instance.value);
+
+        /*
+         * pass by value example
+         */
+        TestPoint tp1 = instance.new TestPoint("TP1");
+        TestPoint tp2 = instance.new TestPoint("TP2");
+        System.out.println("Before badbadSwap, tp1 => " + tp1.name);
+        System.out.println("Before badSwap, tp2 => " + tp2.name);
+        badSwap(tp1, tp2);
+        System.out.println("After badSwap, tp1 => " + tp1.name);
+        System.out.println("After badSwap, tp2 => " + tp2.name);
+
+    }
+
+    public static void badSwap(TestPoint tp1, TestPoint tp2) {
+        TestPoint temp = tp1;
+        tp1 = tp2;
+        tp2 = temp;
     }
 
 }
