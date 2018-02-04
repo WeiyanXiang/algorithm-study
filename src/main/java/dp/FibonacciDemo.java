736aeb50200ac49c959a9d858d79c73649517adf
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-04 18:04:50 +0000

    added more dp impl in fibbonacci demo

--- src/main/java/dp/FibonacciDemo.java
+++ src/main/java/dp/FibonacciDemo.java
@@ -3,8 +3,6 @@
  */
 package dp;
 
-import java.util.Stack;
-
 /**
  * @author weiyan.xiang
  * @date 4 Feb 2018
@@ -15,13 +13,55 @@ public class FibonacciDemo {
         return n <= 1 ? n : fibRecursice(n - 1) + fibRecursice(n - 2);
     }
 
-    // public static int fibBottomUpD(int n) {
-    //
-    // }
+    public static int fibOptimalDp(int n) {
+        if (n < 2)
+            return n;
+        int answer = 0, a = 0, b = 1;
+        for (int i = 2; i <= n; i++) {
+            answer = a + b;
+            b = a;
+            a = answer;
+        }
+        return a + b;
+    }
+
+    /**
+     * use an array to cache already done computations and hence increase
+     * performance
+     * 
+     * @param n
+     * @return
+     */
+    public static int fibBottomUpDp(int n) {
+        if (n < 2)
+            return n;
+        int[] cache = new int[n + 1];
+        cache[0] = 0;
+        cache[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+        return cache[n];
+    }
+
+    public static int fibTopBottomDp(int n) {
+        return doTopBottomFib(n, new int[n + 1]);
+    }
+
+    private static int doTopBottomFib(int n, int[] cache) {
+        if (n < 2)
+            return n;
+        /**
+         * cache the result to avoid repeated computing
+         */
+        cache[n] = cache[n - 1] + cache[n - 2];
+        return doTopBottomFib(n - 1, cache) + doTopBottomFib(n - 2, cache);
+    }
 
     public static void main(String[] args) {
         System.out.println("fibRecursice(18) => " + fibRecursice(18));
-        Stack<Character> stack = new Stack<>();
-        stack.push('c');
+        System.out.println("fibBottomUpDp(18) => " + fibBottomUpDp(18));
+        System.out.println("fibTopBottomDp(18) => " + fibTopBottomDp(18));
+        System.out.println("fibOptimalDp(18) => " + fibOptimalDp(18));
     }
 }
