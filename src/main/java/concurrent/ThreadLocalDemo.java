b208b16a0b5a83554e6912b268c26713699bff2e
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-06 23:42:30 +0000

    added better example in threadLocal demo

--- src/main/java/concurrent/ThreadLocalDemo.java
+++ src/main/java/concurrent/ThreadLocalDemo.java
@@ -3,9 +3,6 @@
  */
 package concurrent;
 
-import static java.lang.Math.random;
-import static java.lang.Math.round;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -14,24 +11,6 @@ import java.util.Date;
  * @date 6 Feb 2018
  */
 public class ThreadLocalDemo {
-
-    public static class SampleThread implements Runnable {
-        private ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
-
-        /**
-         * This example creates a single MyRunnable instance which is passed to
-         * two different threads. Both threads execute the run() method, and
-         * thus sets different values on the ThreadLocal instance. If the access
-         * to the set() call had been synchronized, and it had not been a
-         * ThreadLocal object, the second thread would have overridden the value
-         * set by the first thread.
-         */
-        @Override
-        public void run() {
-            threadLocal.set((int) round(random() * 100));
-            System.out.println("a thread is running, random value is: " + threadLocal.get());
-        }
-    }
 
     /*
      * common usage of threadLocal: You can wrap any non Thread Safe object in
@@ -43,17 +22,19 @@ public class ThreadLocalDemo {
      * 
      * 
      */
-    public static class AnotherSampleThread implements Runnable {
+    public static class SampleThread implements Runnable {
         private ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
             @Override
             protected SimpleDateFormat initialValue() {
+                System.out.println("Thread local created.");
                 return new SimpleDateFormat("yyyyMMdd HHmm");
             }
         };
 
         @Override
         public void run() {
-            System.out.println(this.threadLocal.get().format(new Date()));
+            System.out.println("Thread- " + Thread.currentThread().getName()
+                    + " is running, the thread local formted date: " + threadLocal.get().format(new Date()));
         }
     }
 
