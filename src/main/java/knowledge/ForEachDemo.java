1186f0a3f988281c26b36163e3f5fc9c7034d3e5
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-13 22:43:55 +0000

    added dif foreach demo

---
+++ src/main/java/knowledge/ForEachDemo.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package knowledge;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author weiyan.xiang
+ * @date 13 Feb 2018
+ */
+public class ForEachDemo {
+
+    private static void forEachDemo() {
+        List<Integer> aList = new ArrayList<>(Arrays.asList(1, 2, 4, 2, 5, 2, 5, 1, 4, 21));
+        System.out.println("below is enhanced for each without indexes");
+        for (Integer element : aList) {
+            System.out.print(element + ", ");
+        }
+
+        System.out.println("\nbelow is foreach lambda");
+        aList.forEach(e -> System.out.print(e + ", "));
+    }
+
+    public static void main(String[] args) {
+        forEachDemo();
+    }
+
+}
