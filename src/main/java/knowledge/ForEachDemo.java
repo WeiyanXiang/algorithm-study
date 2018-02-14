9536d2944b27b5b4a430ae08b871ae6f254ea78b
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-14 09:25:50 +0000

    added dif foreach demo

--- src/main/java/knowledge/ForEachDemo.java
+++ src/main/java/knowledge/ForEachDemo.java
@@ -15,7 +15,12 @@ public class ForEachDemo {
 
     private static void forEachDemo() {
         List<Integer> aList = new ArrayList<>(Arrays.asList(1, 2, 4, 2, 5, 2, 5, 1, 4, 21));
-        System.out.println("below is enhanced for each without indexes");
+        System.out.println("below is enhanced for each with indexes");
+        for (int i = 0; i < aList.size(); i++) {
+            System.out.print(aList.get(i) + ", ");
+        }
+
+        System.out.println("\nbelow is enhanced for each without indexes");
         for (Integer element : aList) {
             System.out.print(element + ", ");
         }
