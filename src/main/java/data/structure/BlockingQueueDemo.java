3dbd9ff495de3b9019696a60df001b3fde940cd1
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-15 17:35:44 +0000

    added more actual usage for blocking queue demo

--- src/main/java/data/structure/BlockingQueueDemo.java
+++ src/main/java/data/structure/BlockingQueueDemo.java
@@ -15,48 +15,53 @@ import java.util.concurrent.TimeUnit;
  */
 public class BlockingQueueDemo {
 
-    private static final BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+    private static final BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(20);
 
     public static void main(String[] args) throws InterruptedException {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                queue.add(1);
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        executor.submit(runnable);
+        try {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(1000);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                        queue.add(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-        System.out.println("Below is to poll from blocking queue");
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+                }
+            };
+            executor.submit(runnable);
 
-        Thread t1 = new Thread(runnable);
-        t1.start();
-        System.out.println("Below is to poll from blocking queue");
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
-        System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println("Below is to poll from blocking queue");
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
 
+            executor.submit(runnable);
+            System.out.println("Below is to poll from blocking queue");
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+            System.out.println(queue.poll(200, TimeUnit.MILLISECONDS));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
     }
 
 }
