7f931f562f82760f3d1bc356ad1b4d7cf381f106
Weiyan Xiang <weiyan.xiang@accenture.com>
2018-02-18 01:30:07 +0000

    added more impl for ReentrantLock demo

--- src/main/java/concurrent/ReentrantLockDemo.java
+++ src/main/java/concurrent/ReentrantLockDemo.java
@@ -3,6 +3,7 @@
  */
 package concurrent;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -14,9 +15,44 @@ public class ReentrantLockDemo {
     private static final ReentrantLock lock = new ReentrantLock();
     private static final ReentrantLock fairLock = new ReentrantLock(true);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         normalLockDemo();
+        // System.out.println("---------------------------------------------");
+        // fairLockDemo();
 
+    }
+
+    private static void fairLockDemo() throws InterruptedException {
+        Thread t1 = getThreadWithFairLock("Thread 11", 1000);
+        Thread t2 = getThreadWithFairLock("Thread 22", 800);
+        Thread t3 = getThreadWithFairLock("Thread 33", 600);
+        Thread t4 = getThreadWithFairLock("Thread 44", 400);
+        Thread t5 = getThreadWithFairLock("Thread 55", 200);
+        Thread t6 = getThreadWithFairLock("Thread 66", 0);
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t6.start();
+    }
+
+    private static Thread getThreadWithFairLock(String threadInfo, int waitingLength) {
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                fairLock.lock();
+                try {
+                    System.out.println("Enterring thread: " + threadInfo);
+                    Thread.sleep(waitingLength);
+                    System.out.println("this is " + threadInfo);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    fairLock.unlock();
+                }
+            }
+        });
     }
 
     private static void normalLockDemo() {
@@ -32,11 +68,12 @@ public class ReentrantLockDemo {
         return new Thread(new Runnable() {
             @Override
             public void run() {
-                lock.lock();
                 try {
-                    System.out.println("Enterring thread: " + threadInfo);
-                    Thread.sleep(1000);
-                    System.out.println("this is " + threadInfo);
+                    System.out.println("Enterring thread: " + threadInfo + ", trying to get the lock.");
+                    if (lock.tryLock(800, TimeUnit.MILLISECONDS)) {
+                        Thread.sleep(1000);
+                        System.out.println("this is " + threadInfo);
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
