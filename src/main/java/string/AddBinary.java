6084ecb029b795cab4462fe8b2319729cb2a1fde
Weiyan <ltxfxwy@gmail.com>
2018-03-03 18:59:24 +0000

    added initial impl for add binary

--- src/main/java/string/AddBinary.java
+++ src/main/java/string/AddBinary.java
@@ -8,13 +8,63 @@ package string;
  * @date 1 Mar 2018
  */
 public class AddBinary {
+    /**
+     * Given two binary strings, return their sum (also a binary string).
+     * 
+     * For example,
+     * 
+     * a = "11"
+     * 
+     * b = "1"
+     * 
+     * Return "100".
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
     public static String addBinary(String a, String b) {
         int intA = Integer.parseInt(a);
         int intB = Integer.parseInt(b);
 
-        int raw = intA + intB;
-
-        return toBinary(raw).toString();
+        String answer = "";
+        char digitForNext = '0';
+        for (int i = Math.max(a.length() - 1, b.length()); i >= 0; i--) {
+            if (i < b.length()) {
+                if (digitForNext == '0') {
+                    if (a.charAt(i) == '1' && b.charAt(i) == '1') {
+                        answer += '0';
+                        digitForNext = '1';
+                    } else if (a.charAt(i) == '0' && b.charAt(i) == '0') {
+                        answer += '0';
+                        digitForNext = '0';
+                    } else if (a.charAt(i) == '1' && b.charAt(i) == '0') {
+                        answer += '1';
+                        digitForNext = '0';
+                    } else if (a.charAt(i) == '0' && b.charAt(i) == '1') {
+                        answer += '1';
+                        digitForNext = '0';
+                    }
+                } else {
+                    if (a.charAt(i) == '1' && b.charAt(i) == '1') {
+                        answer += '1';
+                        digitForNext = '1';
+                    } else if (a.charAt(i) == '0' && b.charAt(i) == '0') {
+                        answer += '1';
+                        digitForNext = '0';
+                    } else if (a.charAt(i) == '1' && b.charAt(i) == '0') {
+                        answer += '0';
+                        digitForNext = '1';
+                    } else if (a.charAt(i) == '0' && b.charAt(i) == '1') {
+                        answer += '0';
+                        digitForNext = '1';
+                    }
+                }
+            } else {
+                answer += a.charAt(i);
+            }
+        }
+        return answer;
     }
 
     private static int toBinary(int input) {
@@ -24,11 +74,11 @@ public class AddBinary {
             answer += n % 2;
             answer *= 10;
         }
-        return null;
+        return -1;
     }
 
     public static void main(String[] args) {
-        addBinary("11", "1");
+        System.out.println("100 == " + addBinary("11", "1"));
     }
 
 }
