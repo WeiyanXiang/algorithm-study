af76a22291a310c454ecd0baf3e20de3e60ed344
Weiyan <ltxfxwy@gmail.com>
2018-03-05 15:26:57 +0000

    added initial impl for Remove Duplicates From Sorted Linked List

---
+++ src/main/java/array/RemoveDuplicatesFromSortedLinkedList.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package array;
+
+/**
+ * @author weiyan.xiang
+ * @date 5 Mar 2018
+ */
+public class RemoveDuplicatesFromSortedLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
