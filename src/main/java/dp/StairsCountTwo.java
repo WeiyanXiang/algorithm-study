f8ec82f673800b8e36517a5e3a13e7185c797b53
Weiyan <ltxfxwy@gmail.com>
2018-03-05 15:24:37 +0000

    added stairs count optimal impl

--- src/main/java/dp/StairsCountTwo.java
+++ src/main/java/dp/StairsCountTwo.java
@@ -9,4 +9,19 @@ package dp;
  */
 public class StairsCountTwo {
 
+    public static int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+        int answer = 0, a = 1, b = 2;
+        for (int i = 3; i <= n; i++) {
+            answer = a + b;
+            a = b;
+            b = answer;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("89 == " + climbStairs(10));
+    }
 }
