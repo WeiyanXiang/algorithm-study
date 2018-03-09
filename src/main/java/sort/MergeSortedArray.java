536e9791781e73c4d3313a31758ed49eb6c4089b
Weiyan <ltxfxwy@gmail.com>
2018-03-09 19:00:29 +0000

    added main methods with test cases for merge sorted array

--- src/main/java/sort/MergeSortedArray.java
+++ src/main/java/sort/MergeSortedArray.java
@@ -9,4 +9,33 @@ package sort;
  */
 public class MergeSortedArray {
 
+    /**
+     * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1
+     * as one sorted array.
+     * 
+     * Note: You may assume that nums1 has enough space (size that is greater or
+     * equal to m + n) to hold additional elements from nums2. The number of
+     * elements initialized in nums1 and nums2 are m and n respectively.
+     * 
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[100];
+        nums1[0] = 1;
+        nums1[1] = 3;
+        nums1[2] = 5;
+        nums1[3] = 7;
+        nums1[4] = 9;
+
+        int[] nums2 = { 2, 3, 4, 5, 6, 7 };
+        int m = 5, n = 6;
+        merge(nums1, m, nums2, n);
+    }
 }
