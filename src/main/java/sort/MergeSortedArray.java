a450eba0d3b6f1c6fa1d68f71433dc97309d1680
Weiyan <ltxfxwy@gmail.com>
2018-03-10 21:42:39 +0000

    added impl for merge sorted array

--- src/main/java/sort/MergeSortedArray.java
+++ src/main/java/sort/MergeSortedArray.java
@@ -3,6 +3,8 @@
  */
 package sort;
 
+import sort.utils.SortUtils;
+
 /**
  * @author weiyan.xiang
  * @date 9 Mar 2018
@@ -23,11 +25,22 @@ public class MergeSortedArray {
      * @param n
      */
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        // proceed merging in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[k--] = nums2[j--];
+            } else {
+                nums1[k--] = nums1[i--];
+            }
+        }
+        while (j > 0) {
+            nums1[k--] = nums2[j--];
+        }
     }
 
     public static void main(String[] args) {
-        int[] nums1 = new int[100];
+        int[] nums1 = new int[11];
         nums1[0] = 1;
         nums1[1] = 3;
         nums1[2] = 5;
@@ -37,5 +50,6 @@ public class MergeSortedArray {
         int[] nums2 = { 2, 3, 4, 5, 6, 7 };
         int m = 5, n = 6;
         merge(nums1, m, nums2, n);
+        SortUtils.printArray(nums1);
     }
 }
---
+++ src/main/java/sort/utils/SortUtils.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package sort.utils;
+
+import java.util.Arrays;
+
+/**
+ * @author weiyan.xiang
+ * @date 9 Mar 2018
+ */
+public class SortUtils {
+
+    public static void printArray(int[] nums) {
+        Arrays.stream(nums).forEach(System.out::println);
+    }
+
+}
