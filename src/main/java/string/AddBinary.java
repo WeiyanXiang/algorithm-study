f6b40057fb3d4c1a52deddd3d2c07d20d82eab6c
Weiyan <ltxfxwy@gmail.com>
2018-03-10 22:24:38 +0000

    added correct impl for add binary

--- src/main/java/string/AddBinary.java
+++ src/main/java/string/AddBinary.java
@@ -25,7 +25,7 @@ public class AddBinary {
         while (i >= 0 || j >= 0) {
             int sum = carry;
             if (j >= 0)
-                sum += b.charAt(j--) - '0';
+                sum += b.charAt(j--) - '0'; // formatting digit to int
             if (i >= 0)
                 sum += a.charAt(i--) - '0';
             sb.append(sum % 2);
@@ -38,6 +38,9 @@ public class AddBinary {
 
     public static void main(String[] args) {
         System.out.println("100 == " + addBinary("11", "1"));
+
+        System.out.println('1' - '0');
+        System.out.println('0' - '0');
     }
 
 }
