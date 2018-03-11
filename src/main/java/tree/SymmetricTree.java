b36681e7c6115b9a89bdb275d1a03e668717853f
Weiyan <ltxfxwy@gmail.com>
2018-03-11 22:42:43 +0000

    added initial impl for symmetric tree

---
+++ src/main/java/tree/SymmetricTree.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package tree;
+
+/**
+ * @author weiyan.xiang
+ * @date 11 Mar 2018
+ */
+public class SymmetricTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
