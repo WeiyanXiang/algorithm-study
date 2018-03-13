e3c66cad5c5aae6480d00e9239a2fa3dfa43d00b
Weiyan <ltxfxwy@gmail.com>
2018-03-13 16:48:29 +0000

    added correct iterative approach impl and test cases for Symmetric tree

--- src/main/java/tree/SymmetricTree.java
+++ src/main/java/tree/SymmetricTree.java
@@ -3,6 +3,8 @@
  */
 package tree;
 
+import java.util.Stack;
+
 /**
  * @author weiyan.xiang
  * @date 11 Mar 2018
@@ -38,9 +40,31 @@ public class SymmetricTree {
         Bonus points if you could solve it both recursively and iteratively.
      */
     //@formatter:on 
-
-    public static boolean isSymmetric(TreeNode root) {
+    public static boolean isSymmetricRecur(TreeNode root) {
         return root == null || isSubTreeSymmetric(root.left, root.right);
+    }
+
+    public static boolean isSymmetricIterative(TreeNode root) {
+        if (root == null)
+            return true;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root.left);
+        stack.push(root.right);
+        while (!stack.empty()) {
+            TreeNode first = stack.pop();
+            TreeNode second = stack.pop();
+            if (first == null && second == null) {
+                continue;
+            } else if (first == null || second == null || first.val != second.val) {
+                return false;
+            } else {
+                stack.push(first.left);
+                stack.push(second.right);
+                stack.push(first.right);
+                stack.push(second.left);
+            }
+        }
+        return true;
     }
 
     private static boolean isSubTreeSymmetric(TreeNode left, TreeNode right) {
@@ -54,21 +78,31 @@ public class SymmetricTree {
     }
 
     public static void main(String[] args) {
+        //@formatter:off
+        /*
+                1
+               / \
+              2   2
+             / \ / \
+            3  4 4  3
+        */
+        //@formatter:on
         TreeNode root = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(2);
         TreeNode node4 = new TreeNode(3);
-        TreeNode node5 = new TreeNode(3);
-        TreeNode node6 = null;
-        TreeNode node7 = new TreeNode(2);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node6 = new TreeNode(4);
+        TreeNode node7 = new TreeNode(3);
         root.left = node2;
         root.right = node3;
         node2.left = node4;
-        node2.right = null;
-        node3.left = node7;
-        node3.right = null;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
 
-        System.out.println(isSymmetric(root));
+        System.out.println("true == " + isSymmetricRecur(root));
+        System.out.println("true == " + isSymmetricIterative(root));
     }
 
 }
