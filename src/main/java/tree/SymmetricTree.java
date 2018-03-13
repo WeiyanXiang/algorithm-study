c90256482e8619470318dd11ab188ab0928698bb
Weiyan <ltxfxwy@gmail.com>
2018-03-13 16:32:04 +0000

    added correct recursive approach impl and test cases for Symmetric tree

--- src/main/java/tree/SymmetricTree.java
+++ src/main/java/tree/SymmetricTree.java
@@ -3,9 +3,6 @@
  */
 package tree;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author weiyan.xiang
  * @date 11 Mar 2018
@@ -43,38 +40,33 @@ public class SymmetricTree {
     //@formatter:on 
 
     public static boolean isSymmetric(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        inOrderTraverse(root, list);
-        for (int i = 0; i < list.size() / 2; i++) {
-            if (list.get(i) != list.get(list.size() - i - 1)) {
-                return false;
-            }
-        }
-        return true;
+        return root == null || isSubTreeSymmetric(root.left, root.right);
     }
 
-    private static void inOrderTraverse(TreeNode root, List<Integer> list) {
-        if (root != null) {
-            inOrderTraverse(root.left, list);
-            list.add(root.val);
-            inOrderTraverse(root.right, list);
+    private static boolean isSubTreeSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        } else if (left.val != right.val) {
+            return false;
+        } else {
+            return isSubTreeSymmetric(left.left, right.right) && isSubTreeSymmetric(left.right, right.left);
         }
     }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
         TreeNode node4 = new TreeNode(3);
         TreeNode node5 = new TreeNode(3);
-        TreeNode node6 = new TreeNode(4);
-        TreeNode node7 = new TreeNode(4);
+        TreeNode node6 = null;
+        TreeNode node7 = new TreeNode(2);
         root.left = node2;
         root.right = node3;
         node2.left = node4;
-        node2.right = node6;
+        node2.right = null;
         node3.left = node7;
-        node3.right = node5;
+        node3.right = null;
 
         System.out.println(isSymmetric(root));
     }
