248c6c14dcf53a3c4399aa58e86c4bf342fe3690
Weiyan <ltxfxwy@gmail.com>
2018-03-13 16:01:37 +0000

    added pre order traverse approach impl and test cases for Symmetric tree

--- src/main/java/tree/SymmetricTree.java
+++ src/main/java/tree/SymmetricTree.java
@@ -4,10 +4,7 @@
 package tree;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
-import java.util.Stack;
 
 /**
  * @author weiyan.xiang
@@ -44,47 +41,24 @@ public class SymmetricTree {
         Bonus points if you could solve it both recursively and iteratively.
      */
     //@formatter:on 
+
     public static boolean isSymmetric(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<Integer> levelOrderTraverse = new ArrayList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            /*
-             * poll() removes the present head. For more information on poll()
-             * visit http://www.tutorialspoint.com/java/util/linkedlist_poll.htm
-             */
-            TreeNode tempNode = queue.poll();
-            levelOrderTraverse.add(tempNode.val);
-
-            /* Enqueue left child */
-            if (tempNode.left != null) {
-                queue.add(tempNode.left);
-            }
-
-            /* Enqueue right child */
-            if (tempNode.right != null) {
-                queue.add(tempNode.right);
+        List<Integer> list = new ArrayList<>();
+        inOrderTraverse(root, list);
+        for (int i = 0; i < list.size() / 2; i++) {
+            if (list.get(i) != list.get(list.size() - i - 1)) {
+                return false;
             }
         }
-        return isListSymmetric(levelOrderTraverse);
+        return true;
     }
 
-    private static boolean isListSymmetric(List<Integer> levelOrderTraverse) {
-        if (levelOrderTraverse.size() <= 1)
-            return true;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 1; i < levelOrderTraverse.size(); i++) {
-            Integer element = levelOrderTraverse.get(i);
-            if (stack.isEmpty()) {
-                stack.push(element);
-            } else if (stack.peek() == element) {
-                stack.pop();
-            } else {
-                stack.push(element);
-            }
-
+    private static void inOrderTraverse(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            inOrderTraverse(root.left, list);
+            list.add(root.val);
+            inOrderTraverse(root.right, list);
         }
-        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
@@ -103,7 +77,6 @@ public class SymmetricTree {
         node3.right = node5;
 
         System.out.println(isSymmetric(root));
-
     }
 
 }
