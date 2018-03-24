46c423f4be2c89811157b1f3d56e11374d89a592
Weiyan <ltxfxwy@gmail.com>
2018-03-24 23:18:20 +0000

    added initial impl to balanced BST

--- src/main/java/tree/BalancedBinaryTree.java
+++ src/main/java/tree/BalancedBinaryTree.java
@@ -45,11 +45,36 @@ public class BalancedBinaryTree {
      * @param args
      */
     //@formatter:on
-    public boolean isBalanced(TreeNode root) {
-        return false;
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null)
+            return true;
+        return Math.abs(getDfsLength(root.left) - getDfsLength(root.right)) <= 1;
+    }
+
+    private static int getDfsLength(TreeNode node) {
+        if (node == null)
+            return 0;
+        while (node.left != null || node.right != null) {
+            return 1 + Math.max(getDfsLength(node.left), getDfsLength(node.right));
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node6 = new TreeNode(4);
+        TreeNode node7 = new TreeNode(3);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
 
+        System.out.println(isBalanced(root));
     }
 }
