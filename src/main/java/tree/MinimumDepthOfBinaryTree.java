8e10188f5ee08a1d0ca10c81f93466054a6225cd
Weiyan <ltxfxwy@gmail.com>
2018-03-25 23:53:49 +0100

    added initial impl to mini depth of BST

---
+++ src/main/java/tree/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package tree;
+
+/**
+ * @author weiyan.xiang
+ * @date 25 Mar 2018
+ */
+public class MinimumDepthOfBinaryTree {
+    /**
+     * Given a binary tree, find its minimum depth.
+     * 
+     * The minimum depth is the number of nodes along the shortest path from the
+     * root node down to the nearest leaf node.
+     * 
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
