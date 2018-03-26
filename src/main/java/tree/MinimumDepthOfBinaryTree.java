6ee43aaecf065050de939119de9cb6fe15cc8c77
Weiyan <ltxfxwy@gmail.com>
2018-03-26 22:31:08 +0100

    added more imple to mini depth BST

--- src/main/java/tree/MinimumDepthOfBinaryTree.java
+++ src/main/java/tree/MinimumDepthOfBinaryTree.java
@@ -17,11 +17,34 @@ public class MinimumDepthOfBinaryTree {
      * @param root
      * @return
      */
-    public int minDepth(TreeNode root) {
+    public static int minDepth(TreeNode root) {
         return 0;
     }
 
     public static void main(String[] args) {
+      //@formatter:off
+        /*
+                1
+               / \
+              2   2
+             / \ / \
+            3  4 4  3
+        */
+        //@formatter:on
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(2);
+        TreeNode node4 = new TreeNode(3);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node6 = new TreeNode(4);
+        TreeNode node7 = new TreeNode(3);
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
 
+        System.out.println(minDepth(root));
     }
 }
