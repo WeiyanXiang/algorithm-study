90fdfccf5cec6a78d3ac9313d8a628b29936bfd5
Weiyan <ltxfxwy@gmail.com>
2018-04-19 17:11:12 +0100

    added method signure for pascals triangle

---
+++ src/main/java/array/PascalsTriangle.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package array;
+
+import java.util.List;
+
+/**
+ * @author weiyan.xiang
+ * @date 19 Apr 2018
+ */
+public class PascalsTriangle {
+
+    //@formatter:off
+    /**Given numRows, generate the first numRows of Pascal's triangle.
+
+        For example, given numRows = 5,
+        Return
+        
+        [
+             [1],
+            [1,1],
+           [1,2,1],
+          [1,3,3,1],
+         [1,4,6,4,1]
+        ]
+     * 
+     * @param numRows
+     * @return
+     */
+    //@formatter:on
+    public static List<List<Integer>> generate(int numRows) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
