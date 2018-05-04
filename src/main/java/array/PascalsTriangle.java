669f38295b5c366ed03f65ae725c270ee065adbb
WeiyanXiang <ltxfxwy@gmail.com>
2018-05-04 17:09:14 +0100

    added correct impl to pascals triangle

--- src/main/java/array/PascalsTriangle.java
+++ src/main/java/array/PascalsTriangle.java
@@ -38,20 +38,24 @@ public class PascalsTriangle {
 		for (int i = 1; i <= n; i++) {
 			if (i == 1) {
 				answer.add(Arrays.asList(1));
+			} else if (i == 2) {
+				answer.add(Arrays.asList(1, 1));
 			} else {
-				if (i == 2) {
-					answer.add(Arrays.asList(1, 1));
-				} else {
-					List<Integer> row = new ArrayList<>();
-					row.add(1);
-					for (int j = 0; j < answer.get(i - 1).size() - 1; j++) {
-						int subSum = answer.get(i - 1).get(j) + answer.get(i - 1).get(j);
-						row.add(subSum);
-					}
+				List<Integer> row = new ArrayList<>();
+				row.add(1);
+				int currentIndex = i - 2;
+				System.out.println("current index is " + currentIndex);
+				List<Integer> previousRow = answer.get(currentIndex);
+				for (int j = 0; j < previousRow.size() - 1; j++) {
+					int subSum = previousRow.get(j) + previousRow.get(j + 1);
+					row.add(subSum);
 				}
+				row.add(1);
+				answer.add(row);
 			}
 		}
 		return answer;
+
 	}
 
 	public static void main(String[] args) {
