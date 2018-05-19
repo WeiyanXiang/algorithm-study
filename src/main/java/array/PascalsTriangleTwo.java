ea7dadcab8de539b83cb526d0eb796120de1f784
weiyanxiang <weiyanxiang@github.com>
2018-05-19 12:01:50 +0100

    added initial signiture of

---
+++ src/main/java/array/PascalsTriangleTwo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package array;
+
+import java.util.List;
+
+/**
+ * @author Administrator
+ *
+ */
+public class PascalsTriangleTwo {
+
+	public List<Integer> getRow(int rowIndex) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/main/java/tree/PathSumTwo.java
+++ src/main/java/tree/PathSumTwo.java
@@ -25,7 +25,7 @@ public class PathSumTwo {
 		  11  13  4
 		 /  \    / \
 		7    2  5   1
-		Return:
+		Return:    
 		[
 		   [5,4,11,2],
 		   [5,8,4,5]
