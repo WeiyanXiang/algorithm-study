2a53995c19ee9477cdd433d0884907dee6b082f4
weiyanxiang <weiyanxiang@github.com>
2018-05-19 12:04:45 +0100

    added initial signiture of Pascals Triangle Two

--- src/main/java/array/PascalsTriangleTwo.java
+++ src/main/java/array/PascalsTriangleTwo.java
@@ -10,7 +10,24 @@ import java.util.List;
  *
  */
 public class PascalsTriangleTwo {
+	//@formatter:off    
+	/*
+	 * Given a non-negative index k where k ¡Ü 33, return the kth index row of the Pascal's triangle.
 
+		Note that the row index starts from 0.
+		
+		
+		In Pascal's triangle, each number is the sum of the two numbers directly above it.
+		
+		Example:
+		
+		Input: 3
+		Output: [1,3,3,1]
+		Follow up:
+		
+		Could you optimize your algorithm to use only O(k) extra space?
+	 */
+	//@formatter:on  
 	public List<Integer> getRow(int rowIndex) {
 		return null;
 	}
