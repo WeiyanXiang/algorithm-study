1705d5e98bc75a9f7040f4aa3a299df50f07fb46
WeiyanXiang <ltxfxwy@gmail.com>
2020-03-09 10:50:24 +0000

    added iteration approach to count and say

--- src/main/java/string/CountAndSay.java
+++ src/main/java/string/CountAndSay.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package string;
 
@@ -16,18 +16,44 @@ public class CountAndSay {
      * two times in a row, so print "2 1" for n = 3, digit 2 is seen 1 time and
      * then digit 1 is seen 1 so print "1 2 1 1" for n = 4 you will print
      * "1 1 1 2 2 1"
-     * 
+     *
      * 1. 1
-     * 
+     *
      * 2. 11
-     * 
+     *
      * 3. 21
-     * 
+     *
      * 4. 1211
-     * 
+     *
      * 5. 111221
-     * 
+     *
      */
+    public static String countAndSay(int n) {
+        String output = "1";
+        int i = 1;
+        while (i < n) {
+            output = getCountSayFromString(output);
+            i++;
+        }
+        return output;
+    }
+
+    private static String getCountSayFromString(String output) {
+        int read = 0;
+        int currentCount = 0;
+        StringBuilder currentNum = new StringBuilder();
+        while (read < output.length()) {
+            currentCount++;
+            if (read == output.length() - 1 || output.charAt(read) != output.charAt(read + 1)) {
+                currentNum.append(currentCount).append(output.charAt(read));
+                currentCount = 0;
+            }
+            read++;
+        }
+        return currentNum.toString();
+    }
+
+
     public static String count(int n) {
         if (n == 1)
             return "1";
@@ -50,6 +76,7 @@ public class CountAndSay {
 
     public static void main(String[] args) {
         System.out.println(count(10));
+        System.out.println(countAndSay(10));
     }
 
 }
