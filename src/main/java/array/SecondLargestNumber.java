89c99219914b23997acb48c071282cb8cdb0fbfa
WeiyanXiang <ltxfxwy@gmail.com>
2020-03-20 08:27:52 +0000

    added find sec largest num in array

---
+++ src/main/java/array/SecondLargestNumber.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * @author Weiyan Xiang on 2020/3/20
+ */
+public class SecondLargestNumber {
+
+    private static int solution(int[] array) {
+        if (array == null || array.length == 0) throw new RuntimeException("array cannot be null/empty");
+
+        int first = array[0], second = array[0], temp = first;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > first) {
+                temp = first;
+                first = array[i];
+                second = temp;
+            } else if (array[i] > second && array[i] != first) {
+                second = array[i];
+            }
+        }
+        return second;
+    }
+
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 3, 4, 5, 6, 6, 7, 7};
+        System.out.println(solution(input));
+    }
+}
