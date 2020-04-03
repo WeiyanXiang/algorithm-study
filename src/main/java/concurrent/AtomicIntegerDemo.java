c04b02945ab41f13b1a593154914f7162ea8664d
WeiyanXiang <ltxfxwy@gmail.com>
2020-04-03 17:25:00 +0100

    FEN-2 added atomic integer demo

---
+++ src/main/java/concurrent/AtomicIntegerDemo.java
@@ -0,0 +1,46 @@
+package concurrent;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Weiyan Xiang on 2020/3/26
+ * <p>
+ * https://www.baeldung.com/java-atomic-variables
+ */
+public class AtomicIntegerDemo {
+
+    public static void main(String[] args) {
+    }
+
+    /**
+     * thread safe counter with lock
+     */
+    public class SafeCounterWithLock {
+        private volatile int counter;
+
+        public synchronized void increment() {
+            counter++;
+        }
+    }
+
+    /**
+     * thread safe counter with Atomic integer
+     */
+    public class SafeCounterWithoutLock {
+        private final AtomicInteger counter = new AtomicInteger(0);
+
+        public int getValue() {
+            return counter.get();
+        }
+
+        public void increment() {
+            while (true) {
+                int existingValue = getValue();
+                int newValue = existingValue + 1;
+                if (counter.compareAndSet(existingValue, newValue)) {
+                    return;
+                }
+            }
+        }
+    }
+}
