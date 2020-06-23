debe4e6441ef94e9f2e127c3e6ccd10cdb3b89e6
WeiyanXiang <ltxfxwy@gmail.com>
2020-06-23 08:59:55 +0100

    added candy crush 1d definition and merge intervals

---
+++ src/main/java/array/CandyCrush1D.java
@@ -0,0 +1,63 @@
+package array;
+
+/**
+ * @author Weiyan Xiang on 2020/6/22
+ */
+public class CandyCrush1D {
+
+    /**
+     * https://leetcode.com/discuss/interview-question/380650/Bloomberg-or-Phone-Screen-or-Candy-Crush-1D
+     *
+     * Write a function to crush candy in one dimensional board. In candy crushing games, groups of like items are
+     * removed from the board. In this problem, any sequence of 3 or more like items should be removed and any items
+     * adjacent to that sequence should now be considered adjacent to each other. This process should be repeated as
+     * many time as possible. You should greedily remove characters from left to right.
+     *
+     * Example 1:
+     *
+     * Input: "aaabbbc"
+     * Output: "c"
+     * Explanation:
+     * 1. Remove 3 'a': "aaabbbc" => "bbbc"
+     * 2. Remove 3 'b': "bbbc" => "c"
+     * Example 2:
+     *
+     * Input: "aabbbacd"
+     * Output: "cd"
+     * Explanation:
+     * 1. Remove 3 'b': "aabbbacd" => "aaacd"
+     * 2. Remove 3 'a': "aaacd" => "cd"
+     * Example 3:
+     *
+     * Input: "aabbccddeeedcba"
+     * Output: ""
+     * Explanation:
+     * 1. Remove 3 'e': "aabbccddeeedcba" => "aabbccdddcba"
+     * 2. Remove 3 'd': "aabbccdddcba" => "aabbcccba"
+     * 3. Remove 3 'c': "aabbcccba" => "aabbba"
+     * 4. Remove 3 'b': "aabbba" => "aaa"
+     * 5. Remove 3 'a': "aaa" => ""
+     * Example 4:
+     *
+     * Input: "aaabbbacd"
+     * Output: "acd"
+     * Explanation:
+     * 1. Remove 3 'a': "aaabbbacd" => "bbbacd"
+     * 2. Remove 3 'b': "bbbacd" => "acd"
+     * I solved it with recursion and also discussed the stack based approach.
+     *
+     * Follow-up:
+     * What if you need to find the shortest string after removal?
+     *
+     * Example:
+     *
+     * Input: "aaabbbacd"
+     * Output: "cd"
+     * Explanation:
+     * 1. Remove 3 'b': "aaabbbacd" => "aaaacd"
+     * 2. Remove 4 'a': "aaaacd" => "cd"
+     *
+     */
+
+
+}
---
+++ src/main/java/array/MergeIntervals.java
@@ -0,0 +1,45 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author Weiyan Xiang on 2020/6/21
+ */
+public class MergeIntervals {
+
+    /**
+     * https://leetcode.com/problems/merge-intervals/
+     */
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, Comparator.comparingInt(i1 -> i1[0]));
+        List<int[]> ans = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        ans.add(newInterval);
+        for (int[] interval : intervals) {
+            if (interval[0] <= newInterval[1])
+                newInterval[1] = Math.max(newInterval[1], interval[1]);
+            else {
+                newInterval = interval;
+                ans.add(newInterval);
+            }
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {
+                {1, 3},
+                {2, 6},
+                {5, 10},
+                {11, 18},
+        };
+
+        int[][] merged = new MergeIntervals().merge(intervals);
+        Arrays.stream(merged).forEach(e -> System.out.println(Arrays.toString(e)));
+    }
+
+}
