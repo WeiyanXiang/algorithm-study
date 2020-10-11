efc6834c7f5226c7bd2a311ba26a8e7c0f506315
WeiyanXiang <ltxfxwy@gmail.com>
2020-10-11 10:54:05 +0100

    clean up singleton example code

--- src/main/java/design/pattern/singleton/SingletonImplFifth.java
+++ src/main/java/design/pattern/singleton/SingletonImplFifth.java
@@ -15,6 +15,12 @@ public class SingletonImplFifth {
     private SingletonImplFifth() {
     }
 
+    /**
+     * If your singleton class is not using a lot of resources, this is the approach to use. But in most of the
+     * scenarios, Singleton classes are created for resources such as File System, Database connections etc and we
+     * should avoid the instantiation until unless client calls the getInstance method. Also this method doesn�t provide
+     * any options for exception handling.
+     */
     public SingletonImplFifth getInstance() {
         return INSTANCE;
     }
