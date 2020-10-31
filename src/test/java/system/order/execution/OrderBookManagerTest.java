0e36be8c3d80cdf2ec790a7b0a87f80c5dd71d79
WeiyanXiang <ltxfxwy@gmail.com>
2020-10-31 22:04:50 +0000

    enriched test for OE project

--- src/test/java/system/order/execution/OrderBookManagerTest.java
+++ src/test/java/system/order/execution/OrderBookManagerTest.java
@@ -21,33 +21,33 @@ public class OrderBookManagerTest {
     OrderBookManager testObj = new OrderBookManager();
     //@formatter:off
     /**
-     *     BUY 10 units at 12.23
-     *     BUY 20 units at 12.31
+     *     BUY 10 units at 8.88
+     *     BUY 20 units at 9.99
      *
-     *     SELL 5 units at 13.55
-     *     BUY 5 units at 12.23
+     *     SELL 5 units at 11.11
+     *     BUY 5 units at 8.88
      *
-     *     BUY 15 units at 12.25
-     *     SELL 5 units at 13.31
+     *     BUY 15 units at 9.01
+     *     SELL 5 units at 10.01
      *
-     *     BUY 30 units at 12.25
-     *     SELL 5 units at 13.31
+     *     BUY 30 units at 9.01
+     *     SELL 5 units at 10.01
      */
     //@formatter:on
     @Test
     public void submit() {
         // act
-        testObj.submit(new Order(GOLD, 12.23, 10, BUY));
-        testObj.submit(new Order(GOLD, 12.31, 20, BUY));
+        testObj.submit(new Order(GOLD, 8.88, 10, BUY));
+        testObj.submit(new Order(GOLD, 9.99, 20, BUY));
 
-        testObj.submit(new Order(GOLD, 13.55, 5, SELL));
-        testObj.submit(new Order(GOLD, 12.23, 5, BUY));
+        testObj.submit(new Order(GOLD, 11.11, 5, SELL));
+        testObj.submit(new Order(GOLD, 8.88, 5, BUY));
 
-        testObj.submit(new Order(GOLD, 12.25, 15, BUY));
-        testObj.submit(new Order(GOLD, 13.31, 5, SELL));
+        testObj.submit(new Order(GOLD, 9.01, 15, BUY));
+        testObj.submit(new Order(GOLD, 10.01, 5, SELL));
 
-        testObj.submit(new Order(GOLD, 12.25, 30, BUY));
-        testObj.submit(new Order(GOLD, 13.31, 5, SELL));
+        testObj.submit(new Order(GOLD, 9.01, 30, BUY));
+        testObj.submit(new Order(GOLD, 10.01, 5, SELL));
 
         // print
         System.out.println("Buy side:");
@@ -63,16 +63,16 @@ public class OrderBookManagerTest {
         System.out.println(sellBookList);
 
         // assert
-        assertEquals(12.31, buyBookList.get(0).getPrice(), 0.0001);
+        assertEquals(9.99, buyBookList.get(0).getPrice(), 0.0001);
         assertEquals(20, buyBookList.get(0).getUnit());
-        assertEquals(12.25, buyBookList.get(1).getPrice(), 0.0001);
+        assertEquals(9.01, buyBookList.get(1).getPrice(), 0.0001);
         assertEquals(45, buyBookList.get(1).getUnit());
-        assertEquals(12.23, buyBookList.get(2).getPrice(), 0.0001);
+        assertEquals(8.88, buyBookList.get(2).getPrice(), 0.0001);
         assertEquals(15, buyBookList.get(2).getUnit());
 
-        assertEquals(13.31, sellBookList.get(0).getPrice(), 0.0001);
+        assertEquals(10.01, sellBookList.get(0).getPrice(), 0.0001);
         assertEquals(10, sellBookList.get(0).getUnit());
-        assertEquals(13.55, sellBookList.get(1).getPrice(), 0.0001);
+        assertEquals(11.11, sellBookList.get(1).getPrice(), 0.0001);
         assertEquals(5, sellBookList.get(1).getUnit());
 
     }
