4ca21464ba882cd19fa96c806b7ce248dec8d2d2
WeiyanXiang <ltxfxwy@gmail.com>
2020-11-07 18:26:00 +0000

    added Palindromepartition

--- src/main/java/backtrack/SubsetsWithDup.java
+++ src/main/java/backtrack/SubsetsWithDup.java
@@ -20,6 +20,11 @@ public class SubsetsWithDup {
         return list;
     }
 
+    /**
+     * The difficulty is to handle the duplicates. With inputs as [1a, 1b, 2a], If we don't handle the duplicates, the
+     * results would be: [1a, 1b, 2a], [1b, 1a, 2a]..., so we must make sure 1a goes before 1b to avoid duplicates By
+     * using nums[i-1]==nums[i] && !used[i-1], we can make sure that 1b cannot be choosed before 1a
+     */
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
         list.add(new ArrayList<>(tempList));
         for (int i = start; i < nums.length; i++) {
--- src/test/java/backtrack/PermutationsTest.java
+++ src/test/java/backtrack/PermutationsTest.java
@@ -2,11 +2,11 @@ package backtrack;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Weiyan Xiang on 2020/11/5
@@ -20,13 +20,19 @@ public class PermutationsTest {
         // act
         List<List<Integer>> expected = testObj.permute(nums);
         // assert
-        List<List<Integer>> actual = new ArrayList<>();
-        actual.add(Arrays.asList(1, 2, 3));
-        actual.add(Arrays.asList(1, 3, 2));
-        actual.add(Arrays.asList(2, 1, 3));
-        actual.add(Arrays.asList(2, 3, 1));
-        actual.add(Arrays.asList(3, 1, 2));
-        actual.add(Arrays.asList(3, 2, 1));
-        assertEquals(expected, actual);
+        List<Integer> p1 = Arrays.asList(1, 2, 3);
+        List<Integer> p2 = Arrays.asList(1, 3, 2);
+        List<Integer> p3 = Arrays.asList(2, 1, 3);
+        List<Integer> p4 = Arrays.asList(2, 3, 1);
+        List<Integer> p5 = Arrays.asList(3, 1, 2);
+        List<Integer> p6 = Arrays.asList(3, 2, 1);
+        assertEquals(6, expected.size());
+        assertTrue(expected.contains(p1));
+        assertTrue(expected.contains(p2));
+        assertTrue(expected.contains(p3));
+        assertTrue(expected.contains(p4));
+        assertTrue(expected.contains(p5));
+        assertTrue(expected.contains(p6));
+
     }
 }
