01b0422d06c8c781dbc5fa32573e98fd69cd92ea
WeiyanXiang <ltxfxwy@gmail.com>
2020-12-27 23:15:26 +0000

    fixed bfs impl for WordLadder

--- src/main/java/search/WordLadder.java
+++ src/main/java/search/WordLadder.java
@@ -20,38 +20,7 @@ public class WordLadder {
         // bfs
         int count = 0;
         LinkedList<String> queue = new LinkedList<>();
-        queue.add(bw);
-        while (!queue.isEmpty()) {
-            count++;
-            int qs = queue.size();
-            for (int ss = qs; ss > 0; ss--) {
-                String cur = queue.poll();
-                StringBuilder sb = new StringBuilder();
-                sb.append(cur);
-                for (int i = 0; i < cur.length(); i++) {
-                    for (char j = 'a'; j <= 'z'; j++) {
-                        if (j == cur.charAt(i)) continue;
-                        sb.setCharAt(i, j);
-                        String updatedCur = sb.toString();
-                        if (updatedCur.equals(ew)) return count + 1;
-                        if (!dict.contains(updatedCur)) continue;
-                        dict.remove(updatedCur);
-                        queue.push(updatedCur);
-                    }
-                    sb.setCharAt(i, cur.charAt(i));
-                }
-            }
-        }
-        return 0;
-    }
-
-    public int ladderLength(String bw, String ew, List<String> wl) {
-        // build dict
-        Set<String> dict = new HashSet<>(wl);
-        if (!wl.contains(ew)) return 0;
-        // bfs
-        int count = 0;
-        LinkedList<String> queue = new LinkedList<>();
+        int wordLen = bw.length();
         queue.add(bw);
         while (!queue.isEmpty()) {
             count++;
@@ -59,16 +28,16 @@ public class WordLadder {
             for (int ss = qs; ss > 0; ss--) {
                 String cur = queue.poll();
                 char[] chs = cur.toCharArray();
-                for (int i = 0; i < chs.length; i++) {
+                for (int i = 0; i < wordLen; i++) {
                     char curChar = chs[i];
                     for (char j = 'a'; j <= 'z'; j++) {
-                        if (j == chs[i]) continue;
+                        if (j == curChar) continue;
                         chs[i] = j;
                         String updatedWord = new String(chs);
                         if (updatedWord.equals(ew)) return count + 1;
                         if (!dict.contains(updatedWord)) continue;
                         dict.remove(updatedWord);
-                        queue.push(updatedWord);
+                        queue.add(updatedWord);
                     }
                     chs[i] = curChar;
                 }
