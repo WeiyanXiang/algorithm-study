ab271a181228db2e11a8c0c522602532d8d5653f
WeiyanXiang <ltxfxwy@gmail.com>
2020-12-28 23:31:27 +0000

    added bi-directional bfs impl for WordLadder

--- src/test/java/search/WordLadderTest.java
+++ src/test/java/search/WordLadderTest.java
@@ -16,20 +16,20 @@ public class WordLadderTest {
         WordLadder testObj = new WordLadder();
 
         // act
-//        int actual = testObj.ladderLength("qa", "sq",
-//                Arrays.asList("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av",
-//                        "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po",
-//                        "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb",
-//                        "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di",
-//                        "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn",
-//                        "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"));
+        int actual = testObj.ladderLength("qa", "sq",
+                Arrays.asList("si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av",
+                        "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po",
+                        "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb",
+                        "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di",
+                        "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn",
+                        "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"));
 
 //        int actual = testObj.ladderLength("hit", "cog",
 //                Arrays.asList("hot", "hit", "cog", "dot", "dog"));
 
 
-        int actual = testObj.ladderLength("hit", "cog",
-                Arrays.asList("hot", "cog", "dot", "dog", "hit", "lot", "log"));
+//        int actual = testObj.ladderLengthBfs("hit", "cog",
+//                Arrays.asList("hot", "cog", "dot", "dog", "hit", "lot", "log"));
 
         // assert
         assertEquals(5, actual);
