92a93908b0dc54d2bc807db44ca6ae98d1f47643
WeiyanXiang <ltxfxwy@gmail.com>
2021-01-09 14:23:57 +0000

    added ac impl for PartitionToKEqualSumSubsets

--- src/main/java/search/PartitionToKEqualSumSubsets.java
+++ src/main/java/search/PartitionToKEqualSumSubsets.java
@@ -2,41 +2,38 @@ package search;/**
  * @author Weiyan Xiang on 2021/1/7
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class PartitionToKEqualSumSubsets {
     /**
      * 698. Partition to K Equal Sum Subsets
      * <p>
      * https://leetcode.com/problems/partition-to-k-equal-sum-subsets/
+     *
+     * upvoted ans
      */
     public boolean canPartitionKSubsets(int[] nums, int k) {
-        int sum = 0;
-        for (int n : nums) sum += n;
-        if (sum % k != 0) return false;
-        int dSum = sum / k;
-        Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        List<Integer> bucket = new ArrayList<>();
-        int l = 0, r = nums.length-1, target = dSum;
-        while (l < r) {
-            if (target < 0) return false;
-            // add to bucket if matching
-            if (target == 0) {
-                list.add(bucket);
-                bucket.clear();
-                target = dSum;
-            } else {
-                bucket.add(nums[l]);
-                target -= nums[l];
-                l++;
-            }
-            target-=nums[r];
-            bucket.add(nums[r]);
-            r--;
+        int sum = 0, max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+            sum += num;
         }
-        return true;
+        int target = sum / k;
+        if (sum % k != 0) return false;
+        if (max > target) return false;
+        return dfs(nums, k, new boolean[nums.length], target, 0, 0);
+    }
+
+    private boolean dfs(int[] nums, int k, boolean[] visited, int target, int cur, int start) {
+        if (k == 0) return true;
+        if (target == cur) return dfs(nums, k - 1, visited, target, 0, 0);
+        for (int i = start; i < nums.length; i++) {
+            if (!visited[i] && nums[i] + cur <= target) {
+                visited[i] = true;
+                if (dfs(nums, k, visited, target, cur + nums[i], i + 1)) {
+                    return true;
+                }
+                visited[i] = false;
+            }
+        }
+        return false;
     }
 }
