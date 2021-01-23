a0284a93ae4381459f4db18ddac88ddf0f483015
WeiyanXiang <ltxfxwy@gmail.com>
2021-01-23 23:51:13 +0000

    added ac ans for NQueens

--- src/main/java/search/backtrack/NQueens.java
+++ src/main/java/search/backtrack/NQueens.java
@@ -10,88 +10,77 @@ public class NQueens {
      * 51. N-Queens
      * <p>
      * https://leetcode.com/problems/n-queens/
+     *
+     * upvoted ans, dfs + backtrack
      */
-    List<List<String>> ans = new ArrayList<>();
-
     public List<List<String>> solveNQueens(int n) {
-        List<String> board = new ArrayList<>();
-        getDefaultBoard(n, board);
-        dfs(n, board, 0);
+        List<List<String>> ans = new ArrayList<>();
+        char[][] board = getDefaultBoard(n);
+        dfs(board, 0, ans);
         return ans;
     }
 
-    private void getDefaultBoard(int n, List<String> board) {
+    private char[][] getDefaultBoard(int n) {
+        char[][] board = new char[n][n];
         for (int i = 0; i < n; i++) {
-            String row = "";
             for (int j = 0; j < n; j++) {
-                row += ".";
+                board[i][j] = '.';
             }
-            board.add(row);
         }
+        return board;
     }
 
-    private boolean dfs(int n, List<String> board, int count) {
-        if (count > n) return false;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                char ch = board.get(i).charAt(j);
-                if (ch == '.') {
-                    if (isValid(board, i, j, n)) {
-                        String curRow = board.get(i);
-                        String newRow = curRow.substring(0, j) + "Q" + curRow.substring(j + 1);
-                        board.set(i, newRow);
-                        if (dfs(n, board, count + 1)) {
-                            ans.add(board);
-                            return true;
-                        } else {
-                            String revertedRow = curRow.substring(0, j) + "." + curRow.substring(j + 1);
-                            board.set(i, revertedRow);
-                        }
-                    }
+    private void dfs(char[][] board, int colIndex, List<List<String>> res) {
+        if (colIndex == board.length) {
+            res.add(build(board));
+            return;
+        }
+        for (int i = 0; i < board.length; i++) {
+            if (isValid(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1, res);
+                board[i][colIndex] = '.';
+            }
+        }
+
+    }
+
+    private boolean isValid(char[][] board, int x, int y) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < y; j++) {
+                /**
+                 * The reason it checks (x + j == y + i || x + y == i + j || x == i):
+                 *
+                 * Every time we find a existing 'Q', 3 conditions need to be met before we can place a new 'Q' in the new column:
+                 *
+                 * no confict in columns : self explanatory as we put 'Q' col by col.
+                 * no confict in rows : x == i
+                 * no conflict in diagonals : Math.abs(x-i) == Math.abs(y-j)
+                 * For Math.abs(x-i) == Math.abs(y-j),
+                 * if x > i, y > j , x - i = y - j => x + j = y + i
+                 * if x < i, y < j, i - x = j - y => x + j = y + i
+                 * if x > i, y < j, x - i = j - y => x + y = i + j
+                 * if x < i, y > j, i - x = y - j => x + y = i + j
+                 *
+                 */
+                // if (board[i][j] == 'Q' && (Math.abs(x-i) == Math.abs(y-j) || x == i)) {
+                if (board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i)) {
+                    return false;
                 }
             }
         }
         return true;
     }
 
-    private boolean isValid(List<String> board, int i, int j, int n) {
-        // same row
-        for (int c = 0; c < n; c++) {
-            if (board.get(i).charAt(c) == 'Q') return false;
+    private List<String> build(char[][] board) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            String row = "";
+            for (int j = 0; j < board[0].length; j++) {
+                row += board[i][j];
+            }
+            ans.add(row);
         }
-        // same col
-        for (int r = 0; r < n; r++) {
-            if (board.get(r).charAt(j) == 'Q') return false;
-        }
-        // /
-        int a = i + 1, b = j - 1;
-        while (a >= 0 && b >= 0 && a < n && b < n) {
-            if (board.get(a).charAt(b) == 'Q') return false;
-            a++;
-            b--;
-        }
-        a = i - 1;
-        b = j + 1;
-        while (a >= 0 && b >= 0 && a < n && b < n) {
-            if (board.get(a).charAt(b) == 'Q') return false;
-            a--;
-            b++;
-        }
-        // \
-        a = i - 1;
-        b = j - 1;
-        while (a >= 0 && b >= 0 && a < n && b < n) {
-            if (board.get(a).charAt(b) == 'Q') return false;
-            a--;
-            b--;
-        }
-        a = i + 1;
-        b = j + 1;
-        while (a >= 0 && b >= 0 && a < n && b < n) {
-            if (board.get(a).charAt(b) == 'Q') return false;
-            a++;
-            b++;
-        }
-        return true;
+        return ans;
     }
 }
