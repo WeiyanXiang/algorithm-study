9d7f5c530b717670c67519d1631d527fe28e38c9
WeiyanXiang <ltxfxwy@gmail.com>
2021-01-31 20:38:55 +0000

    added ac ans for NumbersWithSameConsecutiveDifferences

--- src/main/java/search/backtrack/NumbersWithSameConsecutiveDifferences.java
+++ src/main/java/search/backtrack/NumbersWithSameConsecutiveDifferences.java
@@ -10,25 +10,29 @@ public class NumbersWithSameConsecutiveDifferences {
      * 967. Numbers With Same Consecutive Differences
      * <p>
      * https://leetcode.com/problems/numbers-with-same-consecutive-differences/
+     * <p>
+     * my own ac answer
      */
     public int[] numsSameConsecDiff(int n, int k) {
-        List<String> ans = new ArrayList<>();
-        dfs(n, k, ans, "");
-        int[] res = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) res[i] = Integer.valueOf(ans.get(i));
-        return res;
+        List<Integer> ans = new ArrayList<>();
+        dfs(n, k, ans, 0);
+        return ans.stream().mapToInt(i -> i).toArray();
     }
 
-    private void dfs(int n, int k, List<String> ans, String cur) {
-        if (cur.length() == n) {
+    private void dfs(int n, int k, List<Integer> ans, int cur) {
+        if (n == 0) {
             ans.add(cur);
             return;
         }
-        for (char i = '0'; i <= '9'; i++) {
-            if (cur.length() == 0 && i == '0') continue;
-            char lastDigit = cur.length() == 0 ? '0' : cur.toCharArray()[cur.length()-1];
-            if (Math.abs(lastDigit - i) == k) {
-                dfs(n, k, ans, cur + i);
+        for (char i = 0; i <= 9; i++) {
+            if (cur == 0 && i == 0) continue;
+            if (cur == 0) {
+                dfs(n - 1, k, ans, i);
+            } else {
+                int lastDigit = cur % 10;
+                if (Math.abs(lastDigit - i) == k) {
+                    dfs(n - 1, k, ans, cur * 10 + i);
+                }
             }
         }
     }
