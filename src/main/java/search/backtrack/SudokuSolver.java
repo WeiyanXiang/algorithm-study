88834af2c2c7e23a664671c50fac1111c49a6edf
WeiyanXiang <ltxfxwy@gmail.com>
2021-02-08 23:17:28 +0000

    round two backtracking grid Sudoku questions minor refactorings

--- src/main/java/search/backtrack/SudokuSolver.java
+++ src/main/java/search/backtrack/SudokuSolver.java
@@ -19,15 +19,15 @@ public class SudokuSolver {
     }
 
     private boolean dfs(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char d = '1'; d <= '9'; d++) {
-                        if (isValid(board, d, i, j)) {
-                            board[i][j] = d;
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') {
+                    for (char cur = '1'; cur <= '9'; cur++) {
+                        if (isValid(board, cur, r, c)) {
+                            board[r][c] = cur;
                             if (dfs(board)) return true;
-                            // backtrack
-                            else board[i][j] = '.';
+                                // backtrack
+                            else board[r][c] = '.';
                         }
                     }
                     // my thoughts: below return false means above 1->9 all tried but not working meaning the previous value might be wrong
@@ -40,16 +40,16 @@ public class SudokuSolver {
     }
 
 
-    private boolean isValid(char[][] board, char d, int i, int j) {
-        int subR = (i / 3) * 3;
-        int subC = (j / 3) * 3;
+    private boolean isValid(char[][] board, char cur, int r, int c) {
+        int subR = (r / 3) * 3;
+        int subC = (c / 3) * 3;
         for (int t = 0; t < 9; t++) {
-            if (board[i][t] == d) return false;
-            if (board[t][j] == d) return false;
+            if (board[r][t] == cur) return false;
+            if (board[t][c] == cur) return false;
         }
         for (int a = 0; a < 3; a++) {
             for (int b = 0; b < 3; b++) {
-                if (board[a + subR][b + subC] == d) return false;
+                if (board[a + subR][b + subC] == cur) return false;
             }
         }
         return true;
