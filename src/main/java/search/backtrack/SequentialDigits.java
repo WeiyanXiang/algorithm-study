27a42a9991857e53e977c9041d5c4f5ae5a9a007
WeiyanXiang <ltxfxwy@gmail.com>
2021-02-14 23:02:12 +0000

    added ac ans for SequentialDigits

--- src/main/java/search/backtrack/SequentialDigits.java
+++ src/main/java/search/backtrack/SequentialDigits.java
@@ -3,13 +3,36 @@ package search.backtrack;/**
  */
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class SequentialDigits {
     /**
      * https://leetcode.com/problems/sequential-digits/
      * <p>
-     * bruce force TLE my ans
+     * my dfs ac ans
+     */
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            dfs(low, high, ans, i);
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+
+    private void dfs(int low, int high, List<Integer> ans, int cur) {
+        if (cur > high) return;
+        if (low <= cur && cur <= high) {
+            ans.add(cur);
+        }
+        int i = cur % 10 + 1;
+        if (i > 9) return;
+        dfs(low, high, ans, cur * 10 + i);
+    }
+
+    /**
+     * my bruce force TLE ans
      */
     public List<Integer> sequentialDigitsBF(int low, int high) {
         List<Integer> ans = new ArrayList<>();
