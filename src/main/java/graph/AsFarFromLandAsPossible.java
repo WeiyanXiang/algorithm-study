17af36dc136c706927dcebf4e3c8eaa957fd0867
WeiyanXiang <ltxfxwy@gmail.com>
2021-02-27 22:20:47 +0000

    added AsFarFromLandAsPossible

--- src/main/java/graph/AsFarFromLandAsPossible.java
+++ src/main/java/graph/AsFarFromLandAsPossible.java
@@ -13,6 +13,8 @@ public class AsFarFromLandAsPossible {
      * 1162. As Far from Land as Possible
      * <p>
      * https://leetcode.com/problems/as-far-from-land-as-possible/
+     *
+     * my ac BFS ans, not fast
      */
     int x, y;
 
@@ -20,44 +22,45 @@ public class AsFarFromLandAsPossible {
         x = grid.length;
         y = grid[0].length;
         int max = 0;
+        int zeroCount = 0;
         // loop through each water cell, find its distance to the nearest land
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
                 if (grid[i][j] == 0) {
+                    zeroCount++;
                     max = Math.max(max, bfsNearestLand(grid, i, j, new boolean[x][y]));
                 }
             }
         }
-        return max;
+        return zeroCount == 0 || zeroCount == x * y ? -1 : max;
     }
 
     private int bfsNearestLand(int[][] grid, int r, int c, boolean[][] visited) {
         LinkedList<List<Integer>> queue = new LinkedList<>();
         queue.add(Arrays.asList(r, c));
+        visited[r][c] = true;
         int count = 0;
         while (!queue.isEmpty()) {
-            List<Integer> latest = queue.poll();
+            int qSize = queue.size();
+            for (int i = 0; i < qSize; i++) {
+                List<Integer> latest = queue.poll();
+                int row = latest.get(0);
+                int col = latest.get(1);
+                if (grid[row][col] == 1) return count;
+                checkAndAdd(grid, visited, queue, row + 1, col);
+                checkAndAdd(grid, visited, queue, row - 1, col);
+                checkAndAdd(grid, visited, queue, row, col + 1);
+                checkAndAdd(grid, visited, queue, row, col - 1);
+            }
             count++;
-            int row = latest.get(0);
-            int col = latest.get(1);
-            if (grid[row][col] == 1) return count;
-            if (row < 0 || row >= x || col < 0 || col >= y) continue;
-            checkAndAdd(grid, visited, queue, row + 1, col);
-            checkAndAdd(grid, visited, queue, row - 1, col);
-            checkAndAdd(grid, visited, queue, row, col + 1);
-            checkAndAdd(grid, visited, queue, row, col - 1);
         }
         return count;
     }
 
-    private void checkAndAdd(int[][] grid, boolean[][] visited, LinkedList<List<Integer>> queue, int row, int i) {
-        if (isValid(row, i, grid, visited)) {
-            queue.add(Arrays.asList(row, i));
-            visited[row][i] = true;
+    private void checkAndAdd(int[][] grid, boolean[][] visited, LinkedList<List<Integer>> queue, int r, int c) {
+        if (!(r < 0 || r >= x || c < 0 || c >= y) && !visited[r][c]) {
+            queue.add(Arrays.asList(r, c));
+            visited[r][c] = true;
         }
-    }
-
-    private boolean isValid(int row, int col, int[][] grid, boolean[][] visited) {
-        return !(row < 0 || row >= x || col < 0 || col >= y) && grid[row][col] == 0 && !visited[row][col];
     }
 }
