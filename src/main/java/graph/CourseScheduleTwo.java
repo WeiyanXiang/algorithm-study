b520bea1ef2fc4e7a928fb34f74036910a59e582
WeiyanXiang <ltxfxwy@gmail.com>
2021-03-09 21:50:06 +0000

    added CourseScheduleTwo

--- src/main/java/graph/CourseScheduleTwo.java
+++ src/main/java/graph/CourseScheduleTwo.java
@@ -3,8 +3,8 @@ package graph;/**
  */
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class CourseScheduleTwo {
 
@@ -12,45 +12,43 @@ public class CourseScheduleTwo {
      * 210. Course Schedule II
      * <p>
      * https://leetcode.com/problems/course-schedule-ii/
+     *
+     * huahua ans, topological sort
      */
-    private List<Integer> validPath = new ArrayList<>();
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // build a graph with connected components
-        // return false if there is cycle
-        List<Integer>[] g = new ArrayList[numCourses];
-        for (int i = 0; i < numCourses; i++) g[i] = new ArrayList<>();
+
+        List<Integer>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < graph.length; i++) graph[i] = new ArrayList<>();
+        // build graph
         for (int[] p : prerequisites) {
-            g[p[1]].add(p[0]);
+            graph[p[1]].add(p[0]);
         }
-        boolean[] visited = new boolean[numCourses];
-        boolean[] dp = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            if (!dfsIsValid(g, i, visited, dp, new ArrayList<>())) return convertIntegers(validPath);
-        }
-        return convertIntegers(validPath);
-    }
-
-    public int[] convertIntegers(List<Integer> list) {
-        int[] ans = new int[list.size()];
-        IntStream.range(0, list.size()).forEach(idx -> ans[idx] = list.get(idx));
-        return ans;
-    }
-
-    private boolean dfsIsValid(List<Integer>[] g, int cur, boolean[] visited, boolean[] dp, List<Integer> path) {
-        if (visited[cur]) return dp[cur];
-        visited[cur] = true;
-        path.add(cur);
-        for (int next : g[cur]) {
-            if (!dfsIsValid(g, next, visited, dp, path)) {
-                dp[cur] = false;
-                return false;
+        List<Integer> ans = new ArrayList<>();
+        int[] visited = new int[numCourses];
+        // dfs graph to detect cycles
+        for (int z = 0; z < numCourses; z++) {
+            if (dfsHasCycle(z, graph, visited, ans)) {
+                ans.clear();
+                return new int[0];
             }
+
         }
-        dp[cur] = true;
-        validPath = path;
-        return true;
+        Collections.reverse(ans);
+        return ans.stream().mapToInt(i -> i).toArray();
     }
 
+    private boolean dfsHasCycle(int cur, List<Integer>[] graph, int[] visited, List<Integer> ans) {
+        if (visited[cur] == 1) return true;
+        if (visited[cur] == 2) return false;
+        // mark visiting
+        visited[cur] = 1;
+        for (int next : graph[cur]) {
+            if (dfsHasCycle(next, graph, visited, ans)) return true;
+        }
+        // mark visited and add to path
+        visited[cur] = 2;
+        ans.add(cur);
+        return false;
+    }
 
 }
