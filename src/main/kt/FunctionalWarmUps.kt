7c7238fcf6d12dbcde84b5412607fc9b5ef31096
WeiyanXiang <ltxfxwy@gmail.com>
2021-03-13 20:27:37 +0000

    kotlin warm up p5 - funcitonal built-in methods etc

--- src/main/kt/FunctionalWarmUps.kt
+++ src/main/kt/FunctionalWarmUps.kt
@@ -5,5 +5,25 @@ package kt
  */
 fun main(args: Array<String>) {
     val list = listOf("Kotlin", "Java", "C++", "Javascript")
+    list
+            .filterNotNull()
+//            .filter { it.startsWith("J") }
+//            .map { it.length }
+            .associate { it to it.length }
+//            .take(3)
+//            .takeLast(3)
+            .forEach {
+                println("${it.key}, ${it.value}")
+            }
+
+    val map = list.take(3).associate { it to it.length }
+    println(map)
+
+    val lang = list.first()
+    val langLast = list.last()
+//    list.filterNotNull().last()
+    val find = list.filterNotNull().find { it.startsWith("X") }.orEmpty()
+    println(lang)
+    println(find)
 
 }
