b80e7ecc1ae498d9b0d1dc770ba2cd5bec8764f2
WeiyanXiang <ltxfxwy@gmail.com>
2021-04-03 22:11:10 +0100

    added impl for FlowerPlantingWithNoAdj

--- src/main/java/graph/FlowerPlantingWithNoAdj.java
+++ src/main/java/graph/FlowerPlantingWithNoAdj.java
@@ -2,10 +2,45 @@ package graph;/**
  * @author Weiyan Xiang on 2021/4/2
  */
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlowerPlantingWithNoAdj {
+    /**
+     * 1042. Flower Planting With No Adjacent
+     * <p>
+     * https://leetcode.com/problems/flower-planting-with-no-adjacent/
+     * <p>
+     * upvoted ans, the reason we can loop directly is because the question said it is guaranteed an answer exists.
+     */
     public int[] gardenNoAdj(int n, int[][] paths) {
+        // build adj graph 0-indexed based
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < paths.length; i++) {
+            int a = paths[i][0] - 1;
+            int b = paths[i][1] - 1;
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        int[] res = new int[n]; // colors of each node index in graph
+        for (int i = 0; i < n; i++) {
+            int[] colors = new int[5]; // for each node, new a colors array to use index 1-5
+            for (int neiIndex : graph[i]) {
+                // Mark the color as used if neighbor has used it before.
+                colors[res[neiIndex]] = 1;
+            }
+            // now color the available colors
+            for (int c = 4; c >= 1; c--) {
+                // if current color is available, mark res
+                if (colors[c] != 1) {
+                    res[i] = c;
+                }
+            }
 
+        }
+        return res;
     }
 }
