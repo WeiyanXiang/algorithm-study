559ed71995529eb20a4a89afaaaffbfd8d6ad99d
WeiyanXiang <ltxfxwy@gmail.com>
2021-04-05 23:20:36 +0100

    added BusRoutes

---
+++ src/main/java/graph/BusRoutes.java
@@ -0,0 +1,49 @@
+package graph;/**
+ * @author Weiyan Xiang on 2021/4/5
+ */
+
+import java.util.*;
+
+public class BusRoutes {
+    /**
+     * 815. Bus Routes
+     * <p>
+     * https://leetcode.com/problems/bus-routes/
+     * <p>
+     * huahua ans, build map based on key is stop and value is the list of buses and then BFS
+     */
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        // build map: stop - list of buses
+        // bfs to find the path using least bus
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < routes.length; i++) {
+            for (int stop : routes[i]) {
+                if (map.containsKey(stop)) map.get(stop).add(i);
+                else map.put(stop, new ArrayList(Arrays.asList(i)));
+            }
+        }
+        // BFS
+        int[] visited = new int[500];
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+        queue.add(source);
+        if (source == target) return 0;
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            count++;
+            for (int i = 0; i < len; i++) {
+                int cur = queue.poll();
+                for (int nextBus : map.get(cur)) {
+                    if (visited[nextBus] != 0) continue;
+                    visited[nextBus] = 1;
+                    for (int s : routes[nextBus]) {
+                        if (s == target) return count;
+                        queue.add(s);
+                    }
+                }
+            }
+        }
+        return -1;
+
+    }
+}
