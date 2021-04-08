6c0edeb007807137ca4a2c6984a0e09ce7e29791
WeiyanXiang <ltxfxwy@gmail.com>
2021-04-08 22:54:49 +0100

    added complete impl for AllNodesDistanceK

--- src/main/java/tree/AllNodesDistanceK.java
+++ src/main/java/tree/AllNodesDistanceK.java
@@ -2,26 +2,49 @@ package tree;/**
  * @author Weiyan Xiang on 2021/4/7
  */
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AllNodesDistanceK {
     /**
      * 863. All Nodes Distance K in Binary Tree
-     * 
+     * <p>
      * https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
+     * <p>
+     * upvoted ans: prep a map to store from root to target, target having 0 depth, root having the distance
      */
+    Map<Integer, Integer> map = new HashMap<>();
+    List<Integer> ans = new ArrayList<>();
+
     public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        List<Integer> ans = new ArrayList<>();
-        dfs(root, target, K, 0, ans);
+        // prep a map to store from root to target, target having 0 depth, root having the distance
+        prep(root, target);
+        dfs(root, target, K, 0);
         return ans;
     }
 
-    private void dfs(TreeNode root, TreeNode t, int k, int c, List<Integer> ans){
-        if(c==k) {
-            ans.add(t.val);
-            return;
+    private void dfs(TreeNode cur, TreeNode t, int k, int d) {
+        if (cur == null) return;
+        int curD = map.containsKey(cur.val) ? map.get(cur.val) : d + 1;
+        if (curD == k) ans.add(cur.val);
+        dfs(cur.left, t, k, curD);
+        dfs(cur.right, t, k, curD);
+    }
+
+    private int prep(TreeNode cur, TreeNode target) {
+        if (cur == null) return -1;
+        if (cur == target) {
+            map.put(cur.val, 0);
+            return 0;
         }
-        if(t.left!=null) dfs(root,t.left,k,c+1,ans);
-        if(t.right!=null) dfs(root,t.right,k,c+1,ans);
+        int left = prep(cur.left, target);
+        int right = prep(cur.right, target);
+        if (left == -1 && right == -1) return -1;
+
+        int depth = (left >= 0 ? left : right) + 1;
+        map.put(cur.val, depth);
+        return depth;
     }
 }
