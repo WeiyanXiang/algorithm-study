53c2c41fbbe2f9f8c41ebec57769462a2a58a2f3
WeiyanXiang <ltxfxwy@gmail.com>
2021-06-06 16:41:01 +0100

    added correct impl for ConstructBinaryTreeFromPreorderandPostorderTraversal

--- src/main/java/tree/traverse/topdown/ConstructBinaryTreeFromPreorderandPostorderTraversal.java
+++ src/main/java/tree/traverse/topdown/ConstructBinaryTreeFromPreorderandPostorderTraversal.java
@@ -4,39 +4,38 @@ package tree.traverse.topdown;/**
 
 import tree.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConstructBinaryTreeFromPreorderandPostorderTraversal {
 
     /**
+     * 889. Construct Binary Tree from Preorder and Postorder Traversal
+     * <p>
      * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+     * <p>
+     * my ac answer: preorder is ROOT-L-R so to split postorder (L-R-ROOT), find the root index and then +1 to get the
+     * left end index
      */
-    private int preIndex = 0;
+    private int preindex = 0;
+    Map<Integer, Integer> postMap = new HashMap<>();
 
     public TreeNode constructFromPrePost(int[] pre, int[] post) {
-        preIndex = post.length - 1;
-        return dfsBuild(pre, post, 0, post.length - 1);
-    }
-
-    private TreeNode dfsBuild(int[] pre, int[] post, int l, int r) {
-        if (l > r || preIndex < 0) return null;
-        int riValue = pre[preIndex--];
-        int i = findIndex(post, riValue);
-
-        TreeNode right = dfsBuild(pre, post, l + 1, i - 1);
-        TreeNode left = dfsBuild(pre, post, l, i - 1);
-
-        if (i + 1 >= post.length) return null;
-        TreeNode rootNode = new TreeNode(post[i + 1]);
-        rootNode.left = left;
-        rootNode.right = right;
-
-        return rootNode;
-    }
-
-    private int findIndex(int[] arr, int value) {
-        for (int i = 0; i < arr.length; i++) {
-            if (value == arr[i]) return i;
+        for (int i = 0; i < post.length; i++) {
+            postMap.put(post[i], i);
         }
-        return -1;
+        return dfsBuild(pre, 0, pre.length - 1);
+    }
+
+    private TreeNode dfsBuild(int[] pre, int l, int r) {
+        if (l > r) return null;
+        TreeNode rootNode = new TreeNode(pre[preindex]);
+        preindex++;
+        if (l == r) return rootNode; // return the root
+        int mid = postMap.get(pre[preindex]);
+        rootNode.left = dfsBuild(pre, l, mid);
+        rootNode.right = dfsBuild(pre, mid + 1, r - 1);
+        return rootNode;
     }
 
 
