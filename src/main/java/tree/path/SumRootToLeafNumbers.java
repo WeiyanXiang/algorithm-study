a01b86714052d64e1bd7e1c1b3e5e22eb1818a0e
WeiyanXiang <ltxfxwy@gmail.com>
2021-06-10 23:07:28 +0100

    refined SumRootToLeafNumbers

--- src/main/java/tree/path/SumRootToLeafNumbers.java
+++ src/main/java/tree/path/SumRootToLeafNumbers.java
@@ -2,9 +2,6 @@ package tree.path;
 
 import tree.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author Weiyan Xiang on 2020/12/8
  */
@@ -32,34 +29,8 @@ public class SumRootToLeafNumbers {
         if (root.left == null && root.right == null) sum += cur;
         doSum(root.left, cur);
         doSum(root.right, cur);
-        // cur = (cur - root.val) / 10; // this step is can be ignored, it will be called if keeping it but the state of cur is already in stack. so no backtracking happened effectively.
-    }
-
-    /**
-     * my own ac slow answer: using 2d list to store paths and loop again to calculate result
-     */
-    public int sumNumbers(TreeNode root) {
-        if (root == null) return 0;
-        List<List<Integer>> ans = new ArrayList<>();
-        doPathSum(root, 0, ans, new ArrayList<>());
-        int sumNum = 0;
-        for (List<Integer> row : ans) {
-            int cur = 0;
-            for (Integer v : row) {
-                cur = cur * 10 + v;
-            }
-            sumNum += cur;
-        }
-        return sumNum;
-    }
-
-    private void doPathSum(TreeNode root, int cur, List<List<Integer>> ans, ArrayList<Integer> row) {
-        if (root == null) return;
-        row.add(root.val);
-        if (root.left == null && root.right == null) ans.add(new ArrayList<>(row));
-        doPathSum(root.left, cur, ans, row);
-        doPathSum(root.right, cur, ans, row);
-        row.remove(row.size() - 1); // backtrack
+        // cur = (cur - root.val) / 10; // this step is can be ignored, it will be called if keeping it but the
+        // state of cur is already in stack. so no backtracking happened effectively.
     }
 
 
