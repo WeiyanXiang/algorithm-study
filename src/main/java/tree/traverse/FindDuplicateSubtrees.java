52de65087a7373641754597183bdfe0e19340785
WeiyanXiang <ltxfxwy@gmail.com>
2021-07-04 14:49:13 +0100

    completed FindDuplicateSubtrees

--- src/main/java/tree/traverse/FindDuplicateSubtrees.java
+++ src/main/java/tree/traverse/FindDuplicateSubtrees.java
@@ -4,42 +4,33 @@ package tree.traverse;/**
 
 import tree.TreeNode;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FindDuplicateSubtrees {
 
     /**
      * 652. Find Duplicate Subtrees
-     *
+     * <p>
      * https://leetcode.com/problems/find-duplicate-subtrees/
-     *
-     *
      */
-    Map<Integer,List<Integer>> map = new HashMap<>();
-    Set<TreeNode> ans = new HashSet<>();
+    Map<String, Integer> map = new HashMap<>();
+    List<TreeNode> ans = new ArrayList<>();
+
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        // dfs save a map: key: treenode.val; value: root-left-right-in-string
         dfs(root);
-        return new ArrayList<>(ans);
+        return ans;
     }
 
-    private void dfs(TreeNode root){
-        if(root==null) return;
-        List<Integer> curTra = traverseInString(root,new ArrayList<>());
-        if(map.containsKey(root.val)&&curTra.equals(map.get(root.val))) {
-            ans.add(root);
-        }
-        map.put(root.val, curTra);
-        dfs(root.left);
-        dfs(root.right);
-    }
-
-    private List<Integer> traverseInString(TreeNode root, List<Integer> list){
-        if(root==null) return list;
-        list.add(root.val);
-        traverseInString(root.left,list);
-        traverseInString(root.right,list);
-        return list;
+    private String dfs(TreeNode root) {
+        if (root == null) return "";
+        String cur = root.val + "," + dfs(root.left) + "," + dfs(root.right);
+        map.put(cur, map.getOrDefault(cur, 0) + 1);
+        // count is 2 add once guarantees only add duplicate once, if there are 3 nodes, add only once, instead of twice
+        if (map.get(cur) == 2) ans.add(root);
+        return cur;
     }
 
 }
