0228647564841aa9056ff5ad7a3ce15e373f76b6
WeiyanXiang <ltxfxwy@gmail.com>
2021-07-30 22:26:50 +0100

    reviewed AddOneRowTree with another idea

--- src/main/java/tree/treeprune/AddOneRowTree.java
+++ src/main/java/tree/treeprune/AddOneRowTree.java
@@ -17,6 +17,7 @@ public class AddOneRowTree {
     }
 
     private TreeNode dfs(TreeNode root, int val, int curD, int depth, boolean isLeft) {
+        if (root == null) return null;
         if (curD == depth) {
             TreeNode newNode = new TreeNode(val);
             // if it is left, add to left subtree only and vice versa
@@ -24,9 +25,36 @@ public class AddOneRowTree {
             else newNode.right = root;
             return newNode;
         }
-        if (root == null) return null;
         root.left = dfs(root.left, val, curD + 1, depth, true);
         root.right = dfs(root.right, val, curD + 1, depth, false);
+        return root;
+    }
+
+    /**
+     * my another ac ans: check at depth-1 and assign in 1 go instead of passing left flag, this is easier to approach
+     */
+    public TreeNode addOneRowBetter(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        }
+        return dfs(root, val, 1, depth);
+    }
+
+    private TreeNode dfs(TreeNode root, int val, int curD, int depth) {
+        if (root == null) return null;
+        if (curD == depth - 1) {
+            TreeNode l = new TreeNode(val);
+            TreeNode r = new TreeNode(val);
+            l.left = root.left;
+            r.right = root.right;
+            root.left = l;
+            root.right = r;
+            return root;
+        }
+        root.left = dfs(root.left, val, curD + 1, depth);
+        root.right = dfs(root.right, val, curD + 1, depth);
         return root;
     }
 }
