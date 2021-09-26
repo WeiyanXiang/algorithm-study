e0f33f9db201e07223d42c7f807347dec5b510a3
WeiyanXiang <ltxfxwy@gmail.com>
2021-09-26 23:40:19 +0100

    added correct impl for DeleteNodeInBST

--- src/main/java/tree/treeprune/DeleteNodeInBST.java
+++ src/main/java/tree/treeprune/DeleteNodeInBST.java
@@ -9,17 +9,28 @@ public class DeleteNodeInBST {
      * 450. Delete Node in a BST
      * <p>
      * https://leetcode.com/problems/delete-node-in-a-bst/
+     * <p>
+     * upvoted ans: key part is on find the min of right subtree
      */
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) return null;
-        if (root.val < key) root.right = deleteNode(root.right, key);
-        else if (root.val > key) root.left = deleteNode(root.left, key);
-        else {
-            // dfsBuildRight(root.right);
-            TreeNode temp = root.left;
-            root = root.right;
-            if (temp != null) root.left = temp;
+        if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            // node with two children, replace with the inOrder successor(minVal) in the right subtree
+            root.val = findMin(root.right);
+            root.right = deleteNode(root.right, root.val);
         }
         return root;
+
+    }
+
+    private int findMin(TreeNode root) {
+        while (root.left != null) root = root.left;
+        return root.val;
     }
 }
