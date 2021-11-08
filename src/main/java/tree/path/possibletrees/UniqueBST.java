b99a5ea42c97f7389b6734fa55b2ac0998dd9298
WeiyanXiang <ltxfxwy@gmail.com>
2021-11-08 22:09:51 +0000

    refined UniqueBST

--- src/main/java/tree/path/possibletrees/UniqueBST.java
+++ src/main/java/tree/path/possibletrees/UniqueBST.java
@@ -10,20 +10,25 @@ public class UniqueBST {
      * 96. Unique Binary Search Trees
      * <p>
      * https://leetcode.com/problems/unique-binary-search-trees/
-     * <p>
-     * my ac ans: DP idea: dp[i] = sum(  j = {1..i}, dp[j-1]*dp[n-j] )
+     * Taking 1~n as root respectively:
+     *      1 as root: # of trees = F(0) * F(n-1)  // F(0) == 1
+     *      2 as root: # of trees = F(1) * F(n-2)
+     *      3 as root: # of trees = F(2) * F(n-3)
+     *      ...
+     *      n-1 as root: # of trees = F(n-2) * F(1)
+     *      n as root:   # of trees = F(n-1) * F(0)
+     *
+     * So, the formulation is:
+     *      F(n) = F(0) * F(n-1) + F(1) * F(n-2) + F(2) * F(n-3) + ... + F(n-2) * F(1) + F(n-1) * F(0)
      */
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
-        // dp[i] = sum(  j = {1..i}, dp[j-1]*dp[n-j] )
         for (int i = 2; i <= n; i++) {
-            int sum = 0;
             for (int j = 1; j <= i; j++) {
-                sum += dp[j - 1] * dp[i - j];
+                dp[i] += dp[j - 1] * dp[i - j];
             }
-            dp[i] = sum;
         }
         return dp[n];
     }
