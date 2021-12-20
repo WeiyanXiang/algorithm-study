83b9f2a4613ed971b9c4ddb9da5790dc2a7f5874
WeiyanXiang <ltxfxwy@gmail.com>
2021-12-20 23:50:29 +0000

    reviewed FlattenMultilevelDoublyLinkedList

--- src/main/java/linkedlist/FlattenMultilevelDoublyLinkedList.java
+++ src/main/java/linkedlist/FlattenMultilevelDoublyLinkedList.java
@@ -12,29 +12,58 @@ public class FlattenMultilevelDoublyLinkedList {
      * https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
      * <p>
      * my ac ans
+     * <p>
+     * tricky part is to invalidate head's child since done with flatten
      */
     public Node flatten(Node head) {
         // bottom up dfs
-        Node root = head;
-        return convert(root);
-    }
-
-    private Node convert(Node head) {
         if (head == null) return null;
-        if (head.child == null) head.next = convert(head.next);
+        if (head.child == null) head.next = flatten(head.next);
         if (head.child != null) {
-            Node toFlat = convert(head.child);
+            Node flatChild = flatten(head.child);
             // connect tail of flatten
-            Node iToFlat = toFlat;
-            while (iToFlat.next != null) iToFlat = iToFlat.next;
-            Node curNext = head.next;
-            iToFlat.next = curNext;
-            if (curNext != null) curNext.prev = iToFlat;
+            Node curFlatChild = flatChild;
+            while (curFlatChild.next != null) curFlatChild = curFlatChild.next;
+            Node nextNode = head.next;
+            curFlatChild.next = nextNode;
+            if (nextNode != null) nextNode.prev = curFlatChild;
             // connect head of flatten
-            head.next = toFlat;
-            toFlat.prev = head;
+            head.next = flatChild;
+            flatChild.prev = head;
             // invalidate head's child since done with flatten
             head.child = null;
+        }
+        return head;
+    }
+
+    /**
+     * recur only for flatten children
+     */
+    public Node flattenMyAnotherAns(Node head) {
+        if (head == null) return null;
+        Node cur = head;
+        while (cur != null) {
+            if (cur.child != null) {
+                Node nextNode = cur.next;
+                Node child = flatten(cur.child);
+                // connect head of flatten
+                child.prev = cur;
+                cur.next = child;
+                Node curChild = child;
+                while (curChild.next != null) {
+                    curChild = curChild.next;
+                }
+                // connect tail of flatten
+                curChild.next = nextNode;
+                if (nextNode != null) {
+                    nextNode.prev = curChild;
+                }
+                // invalidate cur's child
+                cur.child = null;
+                cur = nextNode;
+            } else {
+                cur = cur.next;
+            }
         }
         return head;
     }
@@ -45,7 +74,4 @@ public class FlattenMultilevelDoublyLinkedList {
         public Node next;
         public Node child;
     }
-
-    ;
-
 }
