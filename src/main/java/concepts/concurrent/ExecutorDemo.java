3dcbe57db7b300cf079acd9a09397e30dfd52d98
WeiyanXiang <ltxfxwy@gmail.com>
2022-02-02 22:40:45 +0000

    reviewed basics p8

--- src/main/java/concepts/concurrent/ExecutorDemo.java
+++ src/main/java/concepts/concurrent/ExecutorDemo.java
@@ -15,9 +15,13 @@ public class ExecutorDemo {
     ExecutorService singlePool = Executors.newSingleThreadExecutor();
     ExecutorService scheduledPool = Executors.newScheduledThreadPool(5);
     ExecutorService scheculedPool2 = Executors.newSingleThreadScheduledExecutor();
-
+    
     ForkJoinPool forkJoinPool = ForkJoinPool.commonPool();
     ForkJoinPool forkJoinPool2 = new ForkJoinPool(5);
-    ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 6000, TimeUnit.SECONDS,
-            new ArrayBlockingQueue<>(10), new ThreadPoolExecutor.CallerRunsPolicy());
+    ThreadPoolExecutor executor = new ThreadPoolExecutor(5,
+            10,
+            6000,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(10),
+            new ThreadPoolExecutor.DiscardOldestPolicy());
 }
