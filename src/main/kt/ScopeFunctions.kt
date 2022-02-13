7536cfe73d623c9a20be2efbd361915fc3aafa69
Weiyan Xiang <weiyanxiang@Dans-Mac-mini.local>
2022-02-13 22:54:12 +0000

    minor refine

--- src/main/kt/ScopeFunctions.kt
+++ src/main/kt/ScopeFunctions.kt
@@ -156,7 +156,7 @@ fun letUseCases() {
     val modifiedFirstItem = numbers.first().let { firstItem ->
         println("The first item of the list is '$firstItem'")
         if (firstItem.length >= 5) firstItem else "!$firstItem!"
-    }.uppercase()
+    }
     println("First item after modifications: '$modifiedFirstItem'")
 }
 
