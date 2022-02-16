a233997f81b485d2cb2388a67634bfcd29bb89f8
WeiyanXiang <ltxfxwy@gmail.com>
2022-02-16 21:51:58 +0000

    added kotlin collections/sequence p2

--- src/main/kt/Collections.kt
+++ src/main/kt/Collections.kt
@@ -25,4 +25,46 @@ fun main() {
     }
     println(yieldNums.take(6).toList())
 
+    /**
+     * The output of this code shows that the filter() and map() functions are called only when building the result list.
+     * So, you first see the line of text “Lengths of..” and then the sequence processing starts. Note that for elements
+     * left after filtering, the map executes before filtering the next element. When the result size reaches 4, the
+     * processing stops because it's the largest possible size that take(4) can return.
+     */
+    val words = "this is a biggest tests for processing sequence vs lists".split(" ")
+    words.asSequence().filter {
+        println("filter: $it")
+        it.length > 3
+    }.map {
+        println("length: ${it.length}")
+        it.length
+    }.take(4).apply {
+        println(this.toList())
+    }
+
+    val nums: Set<Int?> = setOf(1, 2, 3, null)
+    nums.mapIndexed { i, v -> println("index:$i, value:$v") }
+    nums.mapIndexedNotNull() { i, v -> println("index:$i, value:$v") }
+
+    val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key11" to 11)
+    numbersMap.mapKeys { it.key.uppercase() }.also { println(it) }
+    numbersMap.mapValues { it.value * it.value }.also { println(it) }
+
+    /**
+     * If the collections have different sizes, the result of the zip() is the smaller size; the last elements of the
+     * larger collection are not included in the result.
+     */
+    val colors = listOf("red", "brown", "grey")
+    val animals = listOf("fox", "bear", "wolf")
+    val zippedAnimals: List<Pair<String, String>> = colors.zip(animals)
+    println(zippedAnimals)
+
+    val twoAnimals = listOf("fox", "bear")
+    val zippedAnimalsWithFewer = colors zip twoAnimals
+    println(zippedAnimalsWithFewer)
+
+    // When you have a List of Pairs, you can do the reverse transformation – unzipping – that builds two lists from these pairs
+    val numberPairs: List<Pair<String, Int>> = listOf("one" to 1, "two" to 2, "three" to 3, "four" to 4)
+    val unzip: Pair<List<String>, List<Int>> = numberPairs.unzip()
+    println(unzip)
 }
