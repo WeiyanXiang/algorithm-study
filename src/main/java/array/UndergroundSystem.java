50feccfd8931f70d38392f4593c59955c46769ac
WeiyanXiang <ltxfxwy@gmail.com>
2022-04-24 20:50:12 +0100

    added UndergroundSystem

---
+++ src/main/java/array/UndergroundSystem.java
@@ -0,0 +1,51 @@
+package array;/**
+ * @author Weiyan Xiang on 2022/4/24
+ */
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/design-underground-system/
+ * <p>
+ * 1396. Design Underground System
+ * <p>
+ * maps to track necessar info
+ */
+public class UndergroundSystem {
+
+    Map<Integer, List<Integer>> map;
+    Map<Integer, List<String>> stops;
+    Map<String, List<Double>> average;
+
+    public UndergroundSystem() {
+        this.map = new HashMap<>();
+        this.stops = new HashMap<>();
+        this.average = new HashMap<>();
+    }
+
+    public void checkIn(int id, String stationName, int t) {
+        List<Integer> times = new ArrayList<>();
+        times.add(0 - t);
+        map.put(id, times);
+        stops.put(id, new ArrayList<>(Arrays.asList(stationName)));
+    }
+
+    public void checkOut(int id, String stationName, int t) {
+        List<Integer> curTimes = map.get(id);
+        double curTime = (double) curTimes.get(curTimes.size() - 1) + t;
+        List<String> curStops = stops.get(id);
+        String curTrip = curStops.get(curStops.size() - 1) + "->" + stationName;
+        if (average.containsKey(curTrip)) {
+            average.get(curTrip).add(curTime);
+        } else {
+            average.put(curTrip, new ArrayList<>(Arrays.asList(curTime)));
+        }
+        map.get(id).add(t);
+        stops.get(id).add(stationName);
+
+    }
+
+    public double getAverageTime(String startStation, String endStation) {
+        return average.get(startStation + "->" + endStation).stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+    }
+}
